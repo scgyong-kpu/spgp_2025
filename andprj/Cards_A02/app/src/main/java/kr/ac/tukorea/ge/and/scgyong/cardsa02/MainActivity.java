812f3c80823972635d35d93d3c80d911b967a608
KKY@C221 <scgyong@tukorea.ac.kr>
2025-03-19 15:51:55 +0900

    Binding 객체를 이용하여 화면 로딩. 안쓰는 import 삭제.

--- andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa02/MainActivity.java
+++ andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa02/MainActivity.java
@@ -2,17 +2,19 @@ package kr.ac.tukorea.ge.and.scgyong.cardsa02;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import kr.ac.tukorea.ge.and.scgyong.cardsa02.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private @NonNull ActivityMainBinding ui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ui = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(ui.getRoot());
     }
 }
