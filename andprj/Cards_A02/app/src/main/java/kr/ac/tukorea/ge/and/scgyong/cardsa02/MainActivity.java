f243ac7f97d62a59fe384b70d401a1a5b7b5b0a7
KKY@C221 <scgyong@tukorea.ac.kr>
2025-03-19 17:13:41 +0900

    누른 버튼의 image 를 Ace Spade 로 변경.

--- andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa02/MainActivity.java
+++ andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa02/MainActivity.java
@@ -3,6 +3,7 @@ package kr.ac.tukorea.ge.and.scgyong.cardsa02;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
     public void onBtnCard(View view) {
         Log.d("MainActivity", "Btn ID=" + view.getId());
         //Toast.makeText(this, "Btn ID=" + view.getId(), Toast.LENGTH_SHORT).show();
+        ImageButton btn = (ImageButton) view;
+        btn.setImageResource(R.mipmap.card_as);
     }
 }
