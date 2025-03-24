4483ab8f9290f0ac0d12bf0e59ab4f31c54a34f0
KKY@C221 <scgyong@tukorea.ac.kr>
2025-03-24 13:47:29 +0900

    Restart AlertDialog 보여주기. (Builder Pattern)

--- andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa02/MainActivity.java
+++ andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa02/MainActivity.java
@@ -1,11 +1,13 @@
 package kr.ac.tukorea.ge.and.scgyong.cardsa02;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
@@ -99,6 +101,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnRestart(View view) {
-        startGame();
+        //startGame();
+//        AlertDialog dlg = new AlertDialog();
+//        dlg.show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Restart");
+        builder.setMessage("Are you sure to restart the game?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startGame();
+            }
+        });
+        builder.setNegativeButton("No", null);
+        AlertDialog dlg = builder.create();
+        dlg.show();
     }
 }
