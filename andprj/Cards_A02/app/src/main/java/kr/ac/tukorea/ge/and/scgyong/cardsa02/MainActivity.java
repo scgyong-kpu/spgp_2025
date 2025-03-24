7877c965f1d858766f0726c41a2ad33c6db3536b
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-24 18:23:31 +0900

    같은 카드를 눌렀다면 Toast 를 보여주자.

--- andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa02/MainActivity.java
+++ andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa02/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -85,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
 
         ImageButton btn = (ImageButton) view;
         if (btn == previousCardButton) {
+            Toast.makeText(this, R.string.toast_same_card, Toast.LENGTH_SHORT).show();
             return;
         }
         int previousResourceId = 0;
