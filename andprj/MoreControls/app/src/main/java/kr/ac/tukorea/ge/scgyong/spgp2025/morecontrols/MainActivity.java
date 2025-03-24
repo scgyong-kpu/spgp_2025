aab227cd1ae231fc6f6881c3ab5f85372315b2a2
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-24 18:41:46 +0900

    viewBinding 객체를 사용하여 화면 로드.

--- andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/MainActivity.java
+++ andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/MainActivity.java
@@ -2,13 +2,19 @@ package kr.ac.tukorea.ge.scgyong.spgp2025.morecontrols;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import kr.ac.tukorea.ge.scgyong.spgp2025.morecontrols.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private @NonNull ActivityMainBinding ui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ui = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(ui.getRoot());
     }
 }
