b9627238ef5d94c1f106b6965b7e61817759d282
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-24 21:34:53 +0900

    Lazy Initialization 기법을 사용해 본다. Paint 는 이렇게 만들 필요는 없지만, 많은 객체들이 멤버 생성을 지연시킬 때 이 방법을 사용한다.

--- andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/MyView.java
+++ andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/MyView.java
@@ -30,12 +30,18 @@ public class MyView extends View {
         init(attrs, defStyle);
     }
 
-    private Paint paint;
     private void init(AttributeSet attrs, int defStyle) {
-        paint = new Paint();
-        paint.setColor(Color.BLUE);
     }
 
+    // Lazy Initialization
+    private Paint paint;
+    private Paint getPaint() {
+        if (paint == null) {
+            paint = new Paint();
+            paint.setColor(Color.BLUE);
+        }
+        return paint;
+    }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -50,6 +56,6 @@ public class MyView extends View {
         int contentWidth = getWidth() - paddingLeft - paddingRight;
         int contentHeight = getHeight() - paddingTop - paddingBottom;
 
-        canvas.drawOval(paddingLeft, paddingTop, paddingLeft + contentWidth, paddingTop + contentHeight, paint);
+        canvas.drawOval(paddingLeft, paddingTop, paddingLeft + contentWidth, paddingTop + contentHeight, getPaint());
     }
 }
