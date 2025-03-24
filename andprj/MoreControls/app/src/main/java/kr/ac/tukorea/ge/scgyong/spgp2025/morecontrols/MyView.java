cfae965d0d1abc567a25dfbfa13860811b3a2582
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-24 23:02:10 +0900

    Recursive 하게 그려 보려고 하면 당연히 Stack Overflow 에러로 프로그램은 죽는다

--- andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/MyView.java
+++ andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/MyView.java
@@ -85,8 +85,10 @@ public class MyView extends View {
         int eyeY = cy - radius / 4;
         int eyeRadius = radius / 4;
 
-        canvas.drawCircle(leftEyeX, eyeY, eyeRadius, paint);
-        canvas.drawCircle(rightEyeX, eyeY, eyeRadius, paint);
+        drawSmiley(canvas, leftEyeX - eyeRadius, eyeY - eyeRadius, 2 * eyeRadius, 2 * eyeRadius);
+        drawSmiley(canvas, rightEyeX - eyeRadius, eyeY - eyeRadius, 2 * eyeRadius, 2 * eyeRadius);
+//        canvas.drawCircle(leftEyeX, eyeY, eyeRadius, paint);
+//        canvas.drawCircle(rightEyeX, eyeY, eyeRadius, paint);
 
         int mouthX1 = cx - radius / 2, mouthX2 = cx + radius / 2;
         int mouthY = cy + radius / 2;
