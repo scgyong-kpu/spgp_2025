e7739919e29ec51fb9a934d46547e874dd923f65
KKY@C221 <scgyong@tukorea.ac.kr>
2025-03-24 13:51:56 +0900

    Builder Pattern 은 Method Chaining 과 함께 많이 쓰는 편이다.

--- andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa02/MainActivity.java
+++ andprj/Cards_A02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa02/MainActivity.java
@@ -101,20 +101,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnRestart(View view) {
-        //startGame();
-//        AlertDialog dlg = new AlertDialog();
-//        dlg.show();
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Restart");
-        builder.setMessage("Are you sure to restart the game?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                startGame();
-            }
-        });
-        builder.setNegativeButton("No", null);
-        AlertDialog dlg = builder.create();
+        AlertDialog dlg = new AlertDialog.Builder(this)
+                .setTitle("Restart")
+                .setMessage("Are you sure to restart the game?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startGame();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .create();
+
         dlg.show();
     }
 }
