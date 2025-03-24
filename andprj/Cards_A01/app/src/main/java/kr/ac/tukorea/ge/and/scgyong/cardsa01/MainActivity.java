03dc60ed236bdf52103e08aee4bd15e157ba7f97
KKY@C221 <scgyong@tukorea.ac.kr>
2025-03-24 10:56:53 +0900

    셔플을 할 생각이므로 스태틱상수배열에서 멤버변수로 바꾼다

--- andprj/Cards_A01/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa01/MainActivity.java
+++ andprj/Cards_A01/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/cardsa01/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton previousCardButton;
     private @NonNull ActivityMainBinding ui;
 
-    private static final int[] CARD_RES_IDS = {
+    private int[] cardResIds = {
             R.mipmap.card_as, R.mipmap.card_2c, R.mipmap.card_3d, R.mipmap.card_4h,
             R.mipmap.card_5s, R.mipmap.card_jc, R.mipmap.card_qh, R.mipmap.card_kd,
             R.mipmap.card_as, R.mipmap.card_2c, R.mipmap.card_3d, R.mipmap.card_4h,
@@ -36,9 +36,10 @@ public class MainActivity extends AppCompatActivity {
                 ui.card20, ui.card21, ui.card22, ui.card23,
                 ui.card30, ui.card31, ui.card32, ui.card33,
         };
-        for (int i = 0; i < CARD_RES_IDS.length; i++) {
+
+        for (int i = 0; i < cardResIds.length; i++) {
             ImageButton btn = cardButtons[i];
-            int resId = CARD_RES_IDS[i];
+            int resId = cardResIds[i];
             Integer resourceIdInteger = resId;
             btn.setTag(resourceIdInteger);
         }
