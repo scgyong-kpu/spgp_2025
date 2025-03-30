7d0d80d9bd7ec1545669f725fc13b7271a4b74e0
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-30 23:48:18 +0900

    ActionBar 없애기 #1. Java Code 에서 없애 본다. 해당 Activity 에서 없어진다. 하지만 API31 이후인지 여부 확인.

--- andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/AnotherActivity.java
+++ andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/AnotherActivity.java
@@ -2,12 +2,9 @@ package kr.ac.tukorea.ge.scgyong.spgp2025.morecontrols;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import kr.ac.tukorea.ge.scgyong.spgp2025.morecontrols.databinding.ActivityAnotherBinding;
 
@@ -18,6 +15,15 @@ public class AnotherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        // API 31 (Android 12)
+        // Material3 프리뷰 등장 (Theme.Material3)
+        // → 기본적으로 ActionBar 숨김
+        // 위 API31 이후는 위 코드가 null 을 리턴한다
+
         ui = ActivityAnotherBinding.inflate(getLayoutInflater());
         setContentView(ui.getRoot());
     }
