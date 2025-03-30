83ff750bcdbdb15851161634eee76c828cda7ee3
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-30 15:19:57 +0900

    이미지를 추가하고 가운데에 그리는 것을 시도해 본다.

--- andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/BallView.java
+++ andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/BallView.java
@@ -1,12 +1,19 @@
 package kr.ac.tukorea.ge.scgyong.spgp2025.morecontrols;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class BallView extends View {
+    private Bitmap bitmap;
+
     public BallView(Context context) {
         super(context);
         // Java 코드로부터 new BallView 했을 때 불린다
@@ -26,5 +33,15 @@ public class BallView extends View {
     }
     private void init(AttributeSet attrs, int defStyleAttr) {
         // 모든 Constructor 들이 이곳을 통과하게 만들자.
+        Resources res = getResources();
+        bitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+        float cx = getWidth() / 2.0f;
+        float cy = getHeight() / 2.0f;
+        canvas.drawBitmap(bitmap, cx, cy, null);
     }
 }
