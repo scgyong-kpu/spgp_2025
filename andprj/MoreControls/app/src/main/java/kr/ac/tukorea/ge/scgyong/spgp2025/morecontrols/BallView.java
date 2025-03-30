d7ae8ca1777d29476597894f38e557b8a8634749
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-30 15:11:38 +0900

    3종 ctor 가 모두 init() 를 호출하게 하여 실질적인 ctor 의 역할을 하게 한다.

--- andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/BallView.java
+++ andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/BallView.java
@@ -9,13 +9,22 @@ import androidx.annotation.Nullable;
 public class BallView extends View {
     public BallView(Context context) {
         super(context);
+        // Java 코드로부터 new BallView 했을 때 불린다
+        init(null, 0);
     }
 
     public BallView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        // layout xml 로부터 로드될때 속성과 함께 불린다
+        init(attrs, 0);
     }
 
     public BallView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        // layout xml 로부터 로드될때 속성 및 style 과 함께 불린다
+        init(attrs, defStyleAttr);
+    }
+    private void init(AttributeSet attrs, int defStyleAttr) {
+        // 모든 Constructor 들이 이곳을 통과하게 만들자.
     }
 }
