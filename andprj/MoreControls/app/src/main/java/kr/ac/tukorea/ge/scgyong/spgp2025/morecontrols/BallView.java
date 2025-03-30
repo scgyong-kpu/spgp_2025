a9e8e79224803ea89c6a7ecf3cf7aa092cd17461
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-30 15:08:45 +0900

    Alt+Insert (Generate) -> Constructor 를 통해 3종의 Ctor 를 생성해 준다.

--- andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/BallView.java
+++ andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/BallView.java
@@ -1,6 +1,21 @@
 package kr.ac.tukorea.ge.scgyong.spgp2025.morecontrols;
 
+import android.content.Context;
+import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.Nullable;
+
 public class BallView extends View {
+    public BallView(Context context) {
+        super(context);
+    }
+
+    public BallView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BallView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
