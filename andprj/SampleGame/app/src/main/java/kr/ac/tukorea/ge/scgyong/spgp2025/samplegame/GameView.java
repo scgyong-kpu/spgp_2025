88fc07d121b9551070565578ee78f9c308cecc1f
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-31 22:05:30 +0900

    onSizeChanged 에서 matrix 를 구해 두고 그리기 전에 9x16 의 좌표계로 변환하여 그린다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
@@ -13,8 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
-    private static final float SCREEN_WIDTH = 900f;
-    private static final float SCREEN_HEIGHT = 1600f;
+    private static final float SCREEN_WIDTH = 9.0f;
+    private static final float SCREEN_HEIGHT = 16.0f;
     private final Matrix transformMatrix = new Matrix();
     private final RectF borderRect = new RectF(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     private final Paint borderPaint = new Paint();
@@ -32,13 +32,32 @@ public class GameView extends View {
     private void init() {
         // 실질적 생성자 역할
         borderPaint.setStyle(Paint.Style.STROKE);
-        borderPaint.setStrokeWidth(10.0f);
+        borderPaint.setStrokeWidth(0.1f);
         borderPaint.setColor(Color.RED);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        float view_ratio = (float)w / (float)h;
+        float game_ratio = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+        transformMatrix.reset();
+        if (view_ratio > game_ratio) {
+            float scale = h / SCREEN_HEIGHT;
+            transformMatrix.preTranslate((w - h * game_ratio) / 2, 0);
+            transformMatrix.preScale(scale, scale);
+        } else {
+            float scale = w / SCREEN_WIDTH;
+            transformMatrix.preTranslate(0, (h - w / game_ratio) / 2);
+            transformMatrix.preScale(scale, scale);
+        }
     }
 
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
+        canvas.setMatrix(transformMatrix);
         canvas.drawRect(borderRect, borderPaint);
     }
 }
