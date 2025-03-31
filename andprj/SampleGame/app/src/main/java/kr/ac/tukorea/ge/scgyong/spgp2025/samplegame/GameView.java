35ac03c5baa7e0c68e4721f337d9024a03b3216d
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-31 21:57:39 +0900

    에디터를 속이기 위해 멤버변수를 선언만 해도 Preview  가 나온다. 하지만, 생성자를 넣는 방법으로 하자.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
@@ -4,14 +4,10 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 
-import androidx.annotation.Nullable;
-
 public class GameView extends View {
     public GameView(Context context) {
         super(context);
     }
 
-    public GameView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+    AttributeSet set;
 }
