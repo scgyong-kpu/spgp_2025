913de67ea353457b204e6f57676a975d85da29ea
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-31 22:29:23 +0900

    Debug 용 grid 를 그리는 코드를 추가하고 빼고 빌드도 쉽게 할 수 있도록 만든다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
@@ -18,8 +18,6 @@ public class GameView extends View {
     private static final float SCREEN_WIDTH = 9.0f;
     private static final float SCREEN_HEIGHT = 16.0f;
     private final Matrix transformMatrix = new Matrix();
-    private final RectF borderRect = new RectF(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    private final Paint borderPaint = new Paint();
 
     private Bitmap ballBitmap;
     private final RectF ballRect = new RectF(3.5f, 7.0f, 5.5f, 9.0f);
@@ -36,9 +34,6 @@ public class GameView extends View {
 
     private void init() {
         // 실질적 생성자 역할
-        borderPaint.setStyle(Paint.Style.STROKE);
-        borderPaint.setStrokeWidth(0.1f);
-        borderPaint.setColor(Color.RED);
 
         ballBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.soccer_ball_240);
     }
@@ -65,7 +60,33 @@ public class GameView extends View {
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
         canvas.setMatrix(transformMatrix);
-        canvas.drawRect(borderRect, borderPaint);
+        drawDebugBackground(canvas);
         canvas.drawBitmap(ballBitmap, null, ballRect, null);
+    }
+
+    private RectF borderRect;
+    private Paint borderPaint, gridPaint;
+    private void drawDebugBackground(@NonNull Canvas canvas) {
+        if (borderRect == null) {
+            borderRect = new RectF(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+            borderPaint = new Paint();
+            borderPaint.setStyle(Paint.Style.STROKE);
+            borderPaint.setStrokeWidth(0.1f);
+            borderPaint.setColor(Color.RED);
+
+            gridPaint = new Paint();
+            gridPaint.setStyle(Paint.Style.STROKE);
+            gridPaint.setStrokeWidth(0.01f);
+            gridPaint.setColor(Color.GRAY);
+        }
+
+        canvas.drawRect(borderRect, borderPaint);
+        for (float x = 1.0f; x < SCREEN_WIDTH; x += 1.0f) {
+            canvas.drawLine(x, 0, x, SCREEN_HEIGHT, gridPaint);
+        }
+        for (float y = 1.0f; y < SCREEN_HEIGHT; y += 1.0f) {
+            canvas.drawLine(0, y, SCREEN_WIDTH, y, gridPaint);
+        }
     }
 }
