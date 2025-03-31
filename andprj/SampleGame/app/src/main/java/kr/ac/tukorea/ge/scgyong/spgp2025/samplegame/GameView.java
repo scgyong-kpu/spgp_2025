03993073c5656c8baf2cd10d37e49870a65d8762
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-31 21:58:55 +0900

    실질적 생성자인 init() 와 그리기함수 onDraw() 를 만든다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
@@ -1,17 +1,30 @@
 package kr.ac.tukorea.ge.scgyong.spgp2025.samplegame;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
     public GameView(Context context) {
         super(context);
+        init();
     }
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        // 실질적 생성자 역할
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
