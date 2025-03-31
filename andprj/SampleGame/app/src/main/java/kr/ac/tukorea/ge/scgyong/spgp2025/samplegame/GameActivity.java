bfe9c49a5c217ea2e7acb5f1ea6c1e204a014221
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-31 21:10:05 +0900

    Package -> Right Click -> New -> Activity -> Empty Views Activity -> "GameActivity", Layout XML 생성 해제

---
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameActivity.java
@@ -0,0 +1,17 @@
+package kr.ac.tukorea.ge.scgyong.spgp2025.samplegame;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
