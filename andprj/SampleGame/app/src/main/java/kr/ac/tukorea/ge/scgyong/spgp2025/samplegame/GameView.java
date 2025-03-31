b804fc2b3ee2d1d6d49ed753ea573a79206dd8ea
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-31 22:39:37 +0900

    게임 루프에서 불릴 함수이므로 update() 라고 이름을 붙인다. Refactor -> Rename (Shift+F6)

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
@@ -70,12 +70,12 @@ public class GameView extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            move();
+            update();
         }
         return super.onTouchEvent(event);
     }
 
-    private void move() {
+    private void update() {
         ballRect.offset(0.1f, 0.2f);
         Log.d(TAG, "Ball Rect = " + ballRect);
         invalidate();
