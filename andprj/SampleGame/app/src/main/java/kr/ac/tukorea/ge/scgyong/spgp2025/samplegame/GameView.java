85b197616ad056c90200c471e5ed0a013564bdd0
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-31 22:02:55 +0900

    900  x 1600 의 사각형을 그려 본다. 하지만 이 사각형은 다양한 기기에서 제각각의 크기로 나온다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
@@ -2,6 +2,10 @@ package kr.ac.tukorea.ge.scgyong.spgp2025.samplegame;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,6 +13,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    private static final float SCREEN_WIDTH = 900f;
+    private static final float SCREEN_HEIGHT = 1600f;
+    private final Matrix transformMatrix = new Matrix();
+    private final RectF borderRect = new RectF(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    private final Paint borderPaint = new Paint();
+
     public GameView(Context context) {
         super(context);
         init();
@@ -21,10 +31,14 @@ public class GameView extends View {
 
     private void init() {
         // 실질적 생성자 역할
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(10.0f);
+        borderPaint.setColor(Color.RED);
     }
 
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawRect(borderRect, borderPaint);
     }
 }
