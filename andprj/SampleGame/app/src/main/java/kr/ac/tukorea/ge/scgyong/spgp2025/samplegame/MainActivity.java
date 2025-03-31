650b65f8f4ddc6f2f76e0459ac745f66b4c3ece8
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-31 21:14:50 +0900

    MainActivity.java 및 activity_main.xml 정리, 버튼 배치, 클릭시 함수 호출

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/MainActivity.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/MainActivity.java
@@ -1,6 +1,7 @@
 package kr.ac.tukorea.ge.scgyong.spgp2025.samplegame;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public void onBtnStartGame(View view) {
+        // start game activity here
     }
 }
