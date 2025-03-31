cfe5fedc8ce12c6b0a461b34705d7a1293c1033a
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-03-31 21:56:58 +0900

    View 의 Preview 를 보려면 2번째 Constructor 도 있어야 한다. 이건 에디터가 AttributeSet 이라는 단어를 확인하기 때문이다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/GameView.java
@@ -1,12 +1,17 @@
 package kr.ac.tukorea.ge.scgyong.spgp2025.samplegame;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.Nullable;
+
 public class GameView extends View {
-    // Alt + Enter -> Create constructor matching super 또는
-    // Alt + Insert -> Constructor -> View(Context)
     public GameView(Context context) {
         super(context);
+    }
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
     }
 }
