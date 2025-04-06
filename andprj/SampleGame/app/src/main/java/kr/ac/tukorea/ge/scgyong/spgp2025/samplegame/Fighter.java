2dab46d2ded4ca6530b47a77299c365a083b958a
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-07 00:43:51 +0900

    중심 기억 및 회전 적용. 게임 수학에서는 atan2 함수를 많이 사용한다. 각도를 구해서 출력해 보았는데, 문제가 없을까?

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/Fighter.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/Fighter.java
@@ -8,20 +8,31 @@ import android.util.Log;
 public class Fighter {
     private static final String TAG = Fighter.class.getSimpleName();
     private final Bitmap bitmap;
+    private float x, y, angle;
     private final RectF dstRect = new RectF();
 
     public Fighter(Bitmap bitmap) {
         this.bitmap = bitmap;
-        setPosition(5.0f, 12.0f);
+        setPositionOnly(5.0f, 12.0f);
     }
 
     public void draw(Canvas canvas) {
         canvas.drawBitmap(bitmap, null, dstRect, null);
     }
 
-    public void setPosition(float x, float y) {
+    public void setPositionOnly(float x, float y) {
         float r = 1.25f;
         dstRect.set(x-r, y-r, x+r, y+r);
+        this.x = x;
+        this.y = y;
         //Log.d(TAG, "x=" + x + " y=" + y + " rect=" + dstRect);
+    }
+    public void setPosition(float x, float y) {
+        float dx = x - this.x;
+        float dy = y - this.y;
+        double radian = Math.atan2(dy, dx);
+        angle = (float) Math.toDegrees(radian); // intentional bug here
+        Log.d(TAG, "angle=" + angle);
+        setPositionOnly(x, y);
     }
 }
