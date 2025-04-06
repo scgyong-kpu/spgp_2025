b82c70d3c83aea967abbfac73d261f54cb317652
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-07 00:58:07 +0900

    Java 는 function default parameter 를 사용하지 않으므로, overload 를 통해 해결한다. setPosition(x,y) 에서 setPosition(x,y,true) 를 호출하게 하여 외부에서 호출하는 함수 이름을 변경하지 않고도 각도를 적용할지 여부를 정할 수 있게 한다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/Fighter.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/Fighter.java
@@ -13,7 +13,7 @@ public class Fighter {
 
     public Fighter(Bitmap bitmap) {
         this.bitmap = bitmap;
-        setPositionOnly(5.0f, 12.0f);
+        setPosition(5.0f, 12.0f, false);
     }
 
     public void draw(Canvas canvas) {
@@ -23,19 +23,22 @@ public class Fighter {
         canvas.restore();
     }
 
-    public void setPositionOnly(float x, float y) {
+    public void setPosition(float x, float y) {
+        setPosition(x, y, true);
+    }
+    public void setPosition(float x, float y, boolean appliesAngle) {
+        if (appliesAngle) {
+            float dx = x - this.x;
+            float dy = y - this.y;
+            double radian = Math.atan2(dy, dx);
+            angle = (float) Math.toDegrees(radian) + 90;
+            //Log.d(TAG, "angle=" + angle);
+        }
+
         float r = 1.25f;
         dstRect.set(x-r, y-r, x+r, y+r);
         this.x = x;
         this.y = y;
         //Log.d(TAG, "x=" + x + " y=" + y + " rect=" + dstRect);
-    }
-    public void setPosition(float x, float y) {
-        float dx = x - this.x;
-        float dy = y - this.y;
-        double radian = Math.atan2(dy, dx);
-        angle = (float) Math.toDegrees(radian) + 90;
-        Log.d(TAG, "angle=" + angle);
-        setPositionOnly(x, y);
     }
 }
