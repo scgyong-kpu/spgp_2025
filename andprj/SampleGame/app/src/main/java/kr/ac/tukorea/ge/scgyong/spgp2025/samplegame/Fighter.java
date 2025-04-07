9a163c119db0f1447d6b74281be3d692da151cb0
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-08 00:15:05 +0900

    tx 나 ty 를 지나치지 않았는지 체크해야 한다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/Fighter.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/Fighter.java
@@ -27,6 +27,24 @@ public class Fighter {
         float dy = SPEED * (float) Math.sin(radian);
         float x = this.x + dx * GameView.frameTime;
         float y = this.y + dy * GameView.frameTime;
+        if (dx > 0) {
+            if (x > tx) {
+                x = tx;
+            }
+        } else {
+            if (x < tx) {
+                x = tx;
+            }
+        }
+        if (dy > 0) {
+            if (y > ty) {
+                y = ty;
+            }
+        } else {
+            if (y < ty) {
+                y = ty;
+            }
+        }
         setPosition(x, y);
     }
 
