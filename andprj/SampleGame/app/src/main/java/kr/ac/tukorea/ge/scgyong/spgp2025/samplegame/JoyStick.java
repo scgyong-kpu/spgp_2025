ecb4747900f867b511998f13d80bc7460990d40a
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-13 17:24:24 +0900

    Touch Down 인 동안만 보여진다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/JoyStick.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/JoyStick.java
@@ -11,6 +11,9 @@ public class JoyStick implements IGameObject {
 
     private final RectF bgRect = new RectF(0, 1200, 400, 1600);
     private final RectF thumbRect = new RectF(150, 1350, 250, 1450);
+
+    private boolean visible;
+
     public JoyStick() {
         bgBitmap = BitmapPool.get(R.mipmap.joystick_bg);
         thumbBitmap = BitmapPool.get(R.mipmap.joystick_thumb);
@@ -22,6 +25,7 @@ public class JoyStick implements IGameObject {
 
     @Override
     public void draw(Canvas canvas) {
+        if (!visible) return;
         canvas.drawBitmap(bgBitmap, null, bgRect, null);
         canvas.drawBitmap(thumbBitmap, null, thumbRect, null);
     }
@@ -30,6 +34,11 @@ public class JoyStick implements IGameObject {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_MOVE:
+                visible = true;
+                return true;
+            case MotionEvent.ACTION_UP:
+                visible = false;
+                return true;
         }
         return false;
     }
