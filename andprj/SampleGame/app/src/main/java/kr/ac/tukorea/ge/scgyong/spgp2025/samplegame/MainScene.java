476116c6b4d1d1288c117c9001acdc950a99cacd
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-13 10:59:21 +0900

    좌표 100,100 이내에 클릭시 SubScene 으로 진입하게 하려 했는데, gameView 는 어떻게 알지?

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/MainScene.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/MainScene.java
@@ -4,11 +4,13 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.util.Log;
 import android.view.MotionEvent;
 
 import java.util.ArrayList;
 
 public class MainScene extends Scene {
+    private static final String TAG = MainScene.class.getSimpleName();
     private Fighter fighter;
 
     public MainScene(GameView gameView) {
@@ -34,6 +36,12 @@ public class MainScene extends Scene {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_MOVE:
                 float[] xy = Metrics.fromScreen(event.getX(), event.getY());
+                if (xy[0] < 100 && xy[1] < 100) {
+                    Log.d(TAG, "Push SubScene Here. How to get gameView?");
+                    //SubScene subScene = new SubScene();
+                    //gameView.pushScene(subScene);
+                    return false;
+                }
                 fighter.setTargetPosition(xy[0], xy[1]);
                 return true;
         }
---
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/SubScene.java
@@ -0,0 +1,7 @@
+package kr.ac.tukorea.ge.scgyong.spgp2025.samplegame;
+
+public class SubScene extends Scene {
+    public SubScene() {
+        gameObjects.add(new BouncingCircle());
+    }
+}
