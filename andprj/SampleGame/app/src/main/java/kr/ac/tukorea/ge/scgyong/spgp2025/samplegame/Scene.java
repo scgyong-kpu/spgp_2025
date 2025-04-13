b24acec3bb6f609165fdcf7251acea8506606740
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-13 23:46:48 +0900

    ConcurrentModificationException 을 해결하는 한 가지 방법. 하지만 이방법으로 모든게 해결되는 것은 아니다. delay 시키는 방법도 있고 post runnable 시키는 방법도 있다. 이 중 몇 가지는 객체의 갯수가 맞지 않게 되는 문제도 발생한다. 거꾸로 카운트의 경우에도 자신을 삭제하거나 이미 지난 인덱스를 삭제하는 건 괜찮지만 아직 루프에 다다르지 않은 것을 삭제할 때는 여전히 문제이다. 간단한 방법으로 이번 프로젝트/프레임워크 에서 사용하기로 한다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/Scene.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/Scene.java
@@ -16,30 +16,13 @@ public class Scene {
         gameObjects.add(gameObject);
     }
 
-//    Process: kr.ac.tukorea.ge.scgyong.spgp2025.samplegame, PID: 8452
-//    java.util.ConcurrentModificationException
-//        at java.util.ArrayList$Itr.next(ArrayList.java:860)
-//        at kr.ac.tukorea.ge.scgyong.spgp2025.samplegame.Scene.update(Scene.java:23)
-//        at kr.ac.tukorea.ge.scgyong.spgp2025.samplegame.GameView.update(GameView.java:159)
-//        at kr.ac.tukorea.ge.scgyong.spgp2025.samplegame.GameView.doFrame(GameView.java:147)
-//        at android.view.Choreographer$CallbackRecord.run(Choreographer.java:1035)
-//        at android.view.Choreographer.doCallbacks(Choreographer.java:845)
-//        at android.view.Choreographer.doFrame(Choreographer.java:775)
-//        at android.view.Choreographer$FrameDisplayEventReceiver.run(Choreographer.java:1022)
-//        at android.os.Handler.handleCallback(Handler.java:938)
-//        at android.os.Handler.dispatchMessage(Handler.java:99)
-//        at android.os.Looper.loopOnce(Looper.java:201)
-//        at android.os.Looper.loop(Looper.java:288)
-//        at android.app.ActivityThread.main(ActivityThread.java:7839)
-//        at java.lang.reflect.Method.invoke(Native Method)
-//        at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:548)
-//        at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:1003)
-//
     //////////////////////////////////////////////////
     // Game Loop Functions
 
     public void update() {
-        for (IGameObject gobj : gameObjects) {
+        int count = gameObjects.size();
+        for (int i = count - 1; i >= 0; i--) {
+            IGameObject gobj = gameObjects.get(i);
             gobj.update();
         }
     }
