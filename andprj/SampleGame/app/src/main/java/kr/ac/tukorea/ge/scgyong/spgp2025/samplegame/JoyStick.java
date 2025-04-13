7521d6f18453d821c9f7bbb0e88e87e1c60ae606
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-13 23:00:46 +0900

    JoyStick 이 angle 및 power 를 가지게 한다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/JoyStick.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/JoyStick.java
@@ -21,6 +21,7 @@ public class JoyStick implements IGameObject {
 
     private boolean visible;
     private float startX, startY;
+    public float power, angle_radian;
 
     public JoyStick() {
         bgBitmap = BitmapPool.get(R.mipmap.joystick_bg);
@@ -49,25 +50,29 @@ public class JoyStick implements IGameObject {
                 startX = pts[0];
                 startY = pts[1];
                 RectUtil.setRect(thumbRect, CENTER_X, CENTER_Y, THUMB_RADIUS);
-
+                power = 0;
                 return true;
             case MotionEvent.ACTION_MOVE:
                 pts = Metrics.fromScreen(event.getX(), event.getY());
                 float dx = Math.max(-BG_RADIUS, Math.min(pts[0] - startX, BG_RADIUS));
                 float dy = Math.max(-BG_RADIUS, Math.min(pts[1] - startY, BG_RADIUS));
                 double radius = Math.sqrt(dx * dx + dy * dy);
+                angle_radian = (float) Math.atan2(dy, dx);
                 if (radius > MOVE_RADIUS) {
-                    double radian = Math.atan2(dy, dx);
-                    dx = (float) (MOVE_RADIUS * Math.cos(radian));
-                    dy = (float) (MOVE_RADIUS * Math.sin(radian));
+                    dx = (float) (MOVE_RADIUS * Math.cos(angle_radian));
+                    dy = (float) (MOVE_RADIUS * Math.sin(angle_radian));
+                    radius = MOVE_RADIUS;
                 }
+                power = (float) (radius / MOVE_RADIUS);
                 float cx = CENTER_X + dx, cy = CENTER_Y + dy;
-                Log.d(TAG, "sx="+startX+" sy="+startY+" dx="+dx + " dy="+dy);
+                //Log.d(TAG, "sx="+startX+" sy="+startY+" dx="+dx + " dy="+dy);
+                Log.d(TAG, "angle=" + (int)Math.toDegrees(angle_radian) + "° power=" + String.format("%.2f", power));
                 thumbRect.set(cx - THUMB_RADIUS, cy - THUMB_RADIUS, cx + THUMB_RADIUS, cy + THUMB_RADIUS);
                 break;
 
             case MotionEvent.ACTION_UP:
                 visible = false;
+                power = 0;
                 return true;
         }
         return false;
