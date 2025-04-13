c9d79025522972c1ce59d329b6fd4af874982833
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-14 08:47:06 +0900

    Ball 에서 다른 게임오브젝트들이 가질 수 있는 속성들을 예측하여 Sprite 클래스로 만든다. 앞으로 이미지가 있는 객체는 Sprite 를 상속하여 만들 예정이다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/game/Ball.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/game/Ball.java
@@ -1,22 +1,14 @@
 package kr.ac.tukorea.ge.scgyong.spgp2025.samplegame.game;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.RectF;
-
 import java.util.Random;
 
-import kr.ac.tukorea.ge.scgyong.spgp2025.framework.res.BitmapPool;
-import kr.ac.tukorea.ge.scgyong.spgp2025.framework.view.GameView;
-import kr.ac.tukorea.ge.scgyong.spgp2025.framework.interfaces.IGameObject;
+import kr.ac.tukorea.ge.scgyong.spgp2025.framework.objects.Sprite;
 import kr.ac.tukorea.ge.scgyong.spgp2025.framework.view.Metrics;
 import kr.ac.tukorea.ge.scgyong.spgp2025.samplegame.R;
 
-public class Ball implements IGameObject {
-    private final RectF dstRect = new RectF();
+public class Ball extends Sprite {
     private static final float BALL_RADIUS = 100f;
     private static final float SPEED = 700f; // 초당 700 unit 을 움직이는 속도.
-    private float dx, dy;
 
     private static final Random random = new Random();
     public static Ball random() {
@@ -25,21 +17,15 @@ public class Ball implements IGameObject {
                 random.nextFloat() * 360);
     }
     public Ball(float centerX, float centerY, float angle_degree) {
-        dstRect.set(centerX - BALL_RADIUS, centerY - BALL_RADIUS,
-                centerX + BALL_RADIUS, centerY + BALL_RADIUS);
+        super(R.mipmap.soccer_ball_240);
+        setPosition(centerX, centerY, BALL_RADIUS);
         //double radian = Math.PI * angle_degree / 180;
         double radian = Math.toRadians(angle_degree);
         this.dx = SPEED * (float) Math.cos(radian);
         this.dy = SPEED * (float) Math.sin(radian);
-
-        bitmap = BitmapPool.get(R.mipmap.soccer_ball_240);
     }
-    private final Bitmap bitmap;
-
     public void update() {
-        float timedDx = dx * GameView.frameTime;
-        float timedDy = dy * GameView.frameTime;
-        dstRect.offset(timedDx, timedDy);
+        super.update();
         if (dx > 0) {
             if (dstRect.right > Metrics.width) { // Alt+Enter -> Make GameView.SCREEN_WIDTH public
                 dx = -dx;
@@ -58,9 +44,5 @@ public class Ball implements IGameObject {
                 dy = -dy;
             }
         }
-    }
-
-    public void draw(Canvas canvas) {
-        canvas.drawBitmap(bitmap, null, dstRect, null);
     }
 }
