43eb236548a4478bd4cb5e88739fe1d1053151db
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-13 12:57:03 +0900

    Bitmap 을 로드하는 창구가 하나로 정해졌으므로, 모든 비트맵은 화면해상도에 관계 없이 원본 상태 그대로 로드하도록 하는 코드를 적용한다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/BitmapPool.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/BitmapPool.java
@@ -1,5 +1,6 @@
 package kr.ac.tukorea.ge.scgyong.spgp2025.samplegame;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
@@ -9,12 +10,17 @@ import java.util.HashMap;
 public class BitmapPool {
     private static final String TAG = BitmapPool.class.getSimpleName();
     private static final HashMap<Integer, Bitmap> bitmaps = new HashMap<>();
-
+    private static BitmapFactory.Options opts;
     public static Bitmap get(int mipmapResId) {
         Bitmap bitmap = bitmaps.get(mipmapResId);
         if (bitmap == null) {
-            bitmap = BitmapFactory.decodeResource(GameView.view.getResources(), mipmapResId);
-            Log.d(TAG, "Bitmap ID " + mipmapResId + " : " + bitmap.getWidth() + "x" + bitmap.getHeight());
+            if (opts == null) {
+                opts = new BitmapFactory.Options();
+                opts.inScaled = false;
+            }
+            Resources res = GameView.view.getResources();
+            bitmap = BitmapFactory.decodeResource(res, mipmapResId, opts);
+            Log.d(TAG, "Bitmap " + res.getResourceEntryName(mipmapResId) + "(" + mipmapResId + ") : " + bitmap.getWidth() + "x" + bitmap.getHeight());
             bitmaps.put(mipmapResId, bitmap);
         }
         return bitmap;
