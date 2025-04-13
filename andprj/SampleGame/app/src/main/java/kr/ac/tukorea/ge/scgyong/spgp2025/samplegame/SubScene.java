a18cadca8bc4dd422de1a1dac6900df97118c08b
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-13 11:02:39 +0900

    SubScene 에서 아무데나 누르면 pop 되게 한다.

--- andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/SubScene.java
+++ andprj/SampleGame/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/samplegame/SubScene.java
@@ -1,7 +1,15 @@
 package kr.ac.tukorea.ge.scgyong.spgp2025.samplegame;
 
+import android.view.MotionEvent;
+
 public class SubScene extends Scene {
     public SubScene() {
         gameObjects.add(new BouncingCircle());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        GameView.view.popScene();
+        return false;
     }
 }
