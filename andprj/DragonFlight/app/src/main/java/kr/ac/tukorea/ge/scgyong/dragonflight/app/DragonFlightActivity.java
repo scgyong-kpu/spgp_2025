bb04d71852c6399ab1d03c2f0d626ea994d4ef6b
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-15 02:39:28 +0900

    MainScene class 추가하고 화면에 추가. new class 한 다음 Refactor->Move 하여 ...dragonflight.app 에서 ...dragonflight.game 패키지로 옮겨 넣는다.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/app/DragonFlightActivity.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/app/DragonFlightActivity.java
@@ -3,6 +3,7 @@ package kr.ac.tukorea.ge.scgyong.dragonflight.app;
 import android.os.Bundle;
 
 import kr.ac.tukorea.ge.scgyong.dragonflight.BuildConfig;
+import kr.ac.tukorea.ge.scgyong.dragonflight.game.MainScene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 
@@ -12,5 +13,6 @@ public class DragonFlightActivity extends GameActivity {
     protected void onCreate(Bundle savedInstanceState) {
         GameView.drawsDebugStuffs = BuildConfig.DEBUG;
         super.onCreate(savedInstanceState);
+        new MainScene().push();
     }
 }
---
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/MainScene.java
@@ -0,0 +1,8 @@
+package kr.ac.tukorea.ge.scgyong.dragonflight.game;
+
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+
+public class MainScene extends Scene {
+    public MainScene() {
+    }
+}
