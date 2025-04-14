4542e22aa392dea4f2489bb92c572ec2e6b85233
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-15 02:55:18 +0900

    좌/우 만 움직이면 되므로 JoyStick 을 굳이 쓰지 않아도 간단히 구현이 가능하다.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Fighter.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Fighter.java
@@ -1,13 +1,40 @@
 package kr.ac.tukorea.ge.scgyong.dragonflight.game;
 
+import android.view.MotionEvent;
+
 import kr.ac.tukorea.ge.scgyong.dragonflight.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
 public class Fighter extends Sprite {
     private static final float RADIUS = 100f;
+    private static final float SPEED = 300f;
+    private float startX;
+
     public Fighter() {
         super(R.mipmap.fighter);
         setPosition(Metrics.width / 2, Metrics.height - 200, RADIUS);
+    }
+
+    public boolean onTouch(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startX = event.getX();
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                float x = event.getX();
+                if (x < startX) {
+                    dx = -SPEED;
+                } else if (startX < x) {
+                    dx = SPEED;
+                } else {
+                    dx = 0;
+                }
+                return true;
+            case MotionEvent.ACTION_UP:
+                dx = 0;
+                return true;
+        }
+        return false;
     }
 }
--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/MainScene.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/MainScene.java
@@ -1,12 +1,19 @@
 package kr.ac.tukorea.ge.scgyong.dragonflight.game;
 
+import android.view.MotionEvent;
+
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
 public class MainScene extends Scene {
     private final Fighter fighter;
-
     public MainScene() {
         this.fighter = new Fighter();
         add(fighter);
+    }
+
+    // Overridables
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return fighter.onTouch(event);
     }
 }
