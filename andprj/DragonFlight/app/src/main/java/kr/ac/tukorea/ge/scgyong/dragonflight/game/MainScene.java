a210dce7bc302349821e59ecdc08fbdaaf460924
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-15 03:50:10 +0900

    Enemy 생성은 MainScene 이 해도 되지만 EnemyGenerator 가 전담하게 한다.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/MainScene.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/MainScene.java
@@ -2,33 +2,17 @@ package kr.ac.tukorea.ge.scgyong.dragonflight.game;
 
 import android.view.MotionEvent;
 
-import java.util.Random;
-
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
-import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 
 public class MainScene extends Scene {
     private final Fighter fighter;
     public MainScene() {
         this.fighter = new Fighter();
         add(fighter);
+        add(new EnemyGenerator());
     }
 
     // Overridables
-
-    private Random random = new Random();
-    private float enemyTime = 0;
-    @Override
-    public void update() {
-        super.update();
-        enemyTime -= GameView.frameTime;
-        if (enemyTime < 0) {
-            int level = random.nextInt(10);
-            int index = random.nextInt(5);
-            add(new Enemy(level, index));
-            enemyTime = random.nextFloat() + 0.5f;
-        }
-    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
