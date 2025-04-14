122086f4c3f969be8baee91de1c3598e14b8211d
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-15 02:31:54 +0900

    디버그용 grid 및 FPS 표시가 나오도록 하거나

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/app/DragonFlightActivity.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/app/DragonFlightActivity.java
@@ -2,12 +2,15 @@ package kr.ac.tukorea.ge.scgyong.dragonflight.app;
 
 import android.os.Bundle;
 
+import kr.ac.tukorea.ge.scgyong.dragonflight.BuildConfig;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 
 public class DragonFlightActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        GameView.drawsDebugStuffs = true;
         super.onCreate(savedInstanceState);
     }
 }
