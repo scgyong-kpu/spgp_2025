bc27ea7684ebaa047111992b64678e6bdf676733
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-15 03:47:52 +0900

    화면보다 위에서 생성시켜 화면 아래로 사라지면 삭제한다.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Enemy.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Enemy.java
@@ -2,16 +2,26 @@ package kr.ac.tukorea.ge.scgyong.dragonflight.game;
 
 import kr.ac.tukorea.ge.scgyong.dragonflight.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
 public class Enemy extends Sprite {
     private static final float SPEED = 300f;
+    private static final float RADIUS = 80f;
     private static final int[] resIds = {
             R.mipmap.f_01_01, R.mipmap.f_02_01, R.mipmap.f_03_01, R.mipmap.f_04_01, R.mipmap.f_05_01,
             R.mipmap.f_06_01, R.mipmap.f_07_01, R.mipmap.f_08_01, R.mipmap.f_09_01, R.mipmap.f_10_01,
     };
     public Enemy(int level, int index) {
         super(resIds[level]);
-        setPosition(90 * (2 * index + 1), 300f, 160f, 160f);
+        setPosition(90 * (2 * index + 1), -RADIUS, RADIUS);
         dy = SPEED;
+    }
+    @Override
+    public void update() {
+        super.update();
+        if (dstRect.top > Metrics.height) {
+            Scene.top().remove(this);
+        }
     }
 }
