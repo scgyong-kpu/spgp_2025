47afdb0d7d201a37128c38719e57f544842193ea
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-15 02:52:15 +0900

    JoyStick 을 사용하여 비행기 이동. 하지만 2방향인데 굳이 조이스틱을 써야 할까?

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Fighter.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Fighter.java
@@ -1,13 +1,29 @@
 package kr.ac.tukorea.ge.scgyong.dragonflight.game;
 
 import kr.ac.tukorea.ge.scgyong.dragonflight.R;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.JoyStick;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
 public class Fighter extends Sprite {
     private static final float RADIUS = 100f;
-    public Fighter() {
+    private static final float SPEED = 300f;
+    private final JoyStick joyStick;
+
+    public Fighter(JoyStick joyStick) {
         super(R.mipmap.fighter);
         setPosition(Metrics.width / 2, Metrics.height - 200, RADIUS);
+        this.joyStick = joyStick;
+    }
+
+    @Override
+    public void update() {
+        int way = 0;
+        if (joyStick.power > 0) {
+            way = 1 - 2 * (int)Math.round(joyStick.angle_radian / Math.PI);
+        }
+        dx = SPEED * way;
+        super.update();
     }
 }
--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/MainScene.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/MainScene.java
@@ -1,12 +1,25 @@
 package kr.ac.tukorea.ge.scgyong.dragonflight.game;
 
+import android.view.MotionEvent;
+
+import kr.ac.tukorea.ge.scgyong.dragonflight.R;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.JoyStick;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
 public class MainScene extends Scene {
     private final Fighter fighter;
+    private final JoyStick joyStick;
 
     public MainScene() {
-        this.fighter = new Fighter();
+        this.joyStick = new JoyStick(R.mipmap.joystick_bg, R.mipmap.joystick_thumb, 100, 1500, 100, 30, 80);
+        this.fighter = new Fighter(joyStick);
         add(fighter);
+        add(joyStick);
+    }
+
+    // Overridables
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return joyStick.onTouch(event);
     }
 }
