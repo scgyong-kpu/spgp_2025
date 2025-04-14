a1e1409422607a183b5d873c346778e2ff5a0607
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-15 02:14:14 +0900

    Main Activity 꾸미기(?). 각종 Shape 를 사용할 수 있다. 화면 아무데나 터치하면 DragonFlightActivity 로 진입하게 한다.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/app/MainActivity.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/app/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.tukorea.ge.scgyong.dragonflight.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +17,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            startActivity(new Intent(this, DragonFlightActivity.class));
+        }
+        return super.onTouchEvent(event);
     }
 }
