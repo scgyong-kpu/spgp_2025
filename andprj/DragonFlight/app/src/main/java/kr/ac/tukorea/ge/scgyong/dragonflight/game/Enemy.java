720d33f5bc9d3b1cd3b51b5b09447da748e6606e
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-15 04:16:22 +0900

    Metrics 의 값을 이용하도록 한다.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Enemy.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Enemy.java
@@ -7,14 +7,14 @@ import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
 public class Enemy extends Sprite {
     private static final float SPEED = 300f;
-    private static final float RADIUS = 80f;
+    private static final float RADIUS = 90f;
     private static final int[] resIds = {
             R.mipmap.f_01_01, R.mipmap.f_02_01, R.mipmap.f_03_01, R.mipmap.f_04_01, R.mipmap.f_05_01,
             R.mipmap.f_06_01, R.mipmap.f_07_01, R.mipmap.f_08_01, R.mipmap.f_09_01, R.mipmap.f_10_01,
     };
     public Enemy(int level, int index) {
         super(resIds[level]);
-        setPosition(90 * (2 * index + 1), -RADIUS, RADIUS);
+        setPosition(Metrics.width / 10 * (2 * index + 1), -RADIUS, RADIUS);
         dy = SPEED;
     }
     @Override
