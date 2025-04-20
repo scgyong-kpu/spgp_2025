f53457aecdcad9db6c16737201882182f8b07208
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-20 22:53:59 +0900

    a2dg: super 생성자 를 호출했으므로 this.bitmap 값이 설정되었을 것이다. 이미지의 폭과 높이를 알아내자.

--- andprj/DragonFlight/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/objects/AnimSprite.java
+++ andprj/DragonFlight/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/objects/AnimSprite.java
@@ -3,9 +3,20 @@ package kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects;
 public class AnimSprite extends Sprite {
     protected final float fps;
     protected final int frameCount;
+    protected final int frameWidth, frameHeight;
     public AnimSprite(int mipmapId, float fps, int frameCount) {
         super(mipmapId);
         this.fps = fps;
-        this.frameCount = frameCount;
+        int imageWidth = bitmap.getWidth();
+        int imageHeight = bitmap.getHeight();
+        if (frameCount == 0) {
+            this.frameWidth = imageHeight;
+            this.frameHeight = imageHeight;
+            this.frameCount = imageWidth / imageHeight;
+        } else {
+            this.frameWidth = imageWidth / frameCount;
+            this.frameHeight = imageHeight;
+            this.frameCount = frameCount;
+        }
     }
 }
