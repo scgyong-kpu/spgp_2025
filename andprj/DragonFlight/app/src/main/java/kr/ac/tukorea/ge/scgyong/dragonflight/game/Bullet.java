ac4936363f3e6fde733605e79685ed450dec7e75
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-21 05:25:01 +0900

    a2dg: 객체재활용 #3. 각 객체가 pool 을 가지고 있는 것을 시도해 본다. Bullet 에 object pool 을 적용한다. 삭제할 bullet 은 objPool 에 넣으며, 생성시 objPool 을 먼저 뒤져보고 재활용한다. 장단점이 있지만, 최후에는 이 방법을 사용하지 않을 예정이다.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Bullet.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Bullet.java
@@ -1,5 +1,7 @@
 package kr.ac.tukorea.ge.scgyong.dragonflight.game;
 
+import java.util.ArrayList;
+
 import kr.ac.tukorea.ge.scgyong.dragonflight.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
@@ -8,12 +10,18 @@ public class Bullet extends Sprite {
     private static final float BULLET_WIDTH = 68f;
     private static final float BULLET_HEIGHT = BULLET_WIDTH * 40 / 28;
     private static final float SPEED = 2000f;
+    protected static ArrayList<Bullet> objPool = new ArrayList<>();
     private Bullet(float x, float y) {
         super(R.mipmap.laser_1);
         setPosition(x, y, BULLET_WIDTH, BULLET_HEIGHT);
         dy = -SPEED;
     }
     public static Bullet get(float x, float y) {
+        if (!objPool.isEmpty()) {
+            Bullet bullet = objPool.remove(0);
+            bullet.setPosition(x, y, BULLET_WIDTH, BULLET_HEIGHT);
+            return bullet;
+        }
         return new Bullet(x, y);
     }
     @Override
@@ -21,6 +29,7 @@ public class Bullet extends Sprite {
         super.update();
         if (dstRect.bottom < 0) {
             Scene.top().remove(this);
+            objPool.add(this);
         }
     }
 }
