71a20acc12b8ca4c0b2e480b2bcb3f50e79c62a0
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-20 22:36:07 +0900

    Wave Debug 를 위해 시간이 5배 빠르게 가도록 해본다

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/EnemyGenerator.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/EnemyGenerator.java
@@ -17,7 +17,7 @@ public class EnemyGenerator implements IGameObject {
     private int wave;
     @Override
     public void update() {
-        enemyTime -= GameView.frameTime;
+        enemyTime -= GameView.frameTime * 5;
         if (enemyTime < 0) {
             generate();
             enemyTime = GEN_INTERVAL;
