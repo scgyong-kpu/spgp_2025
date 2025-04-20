bdcd4ec169b2118433574e04abc9c21ad501e0bf
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-20 23:11:25 +0900

    Enemy 가 1 프레임짜리 still image 인 f_01_01.png ~ 10 까지 쓰던 것을 enemy_01 ~ enemy_20 까지 애니메이션 이미지로 교체. 이를 위해 Enemy 가 Sprite 가 아닌 AnimSprite 를 상속한다.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Enemy.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Enemy.java
@@ -1,20 +1,22 @@
 package kr.ac.tukorea.ge.scgyong.dragonflight.game;
 
 import kr.ac.tukorea.ge.scgyong.dragonflight.R;
-import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.AnimSprite;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
-public class Enemy extends Sprite {
+public class Enemy extends AnimSprite {
     private static final float SPEED = 300f;
     private static final float RADIUS = 90f;
     private static final int[] resIds = {
-            R.mipmap.f_01_01, R.mipmap.f_02_01, R.mipmap.f_03_01, R.mipmap.f_04_01, R.mipmap.f_05_01,
-            R.mipmap.f_06_01, R.mipmap.f_07_01, R.mipmap.f_08_01, R.mipmap.f_09_01, R.mipmap.f_10_01,
+            R.mipmap.enemy_01, R.mipmap.enemy_02, R.mipmap.enemy_03, R.mipmap.enemy_04, R.mipmap.enemy_05,
+            R.mipmap.enemy_06, R.mipmap.enemy_07, R.mipmap.enemy_08, R.mipmap.enemy_09, R.mipmap.enemy_10,
+            R.mipmap.enemy_11, R.mipmap.enemy_12, R.mipmap.enemy_13, R.mipmap.enemy_14, R.mipmap.enemy_15,
+            R.mipmap.enemy_16, R.mipmap.enemy_17, R.mipmap.enemy_18, R.mipmap.enemy_19, R.mipmap.enemy_20,
     };
     public static final int MAX_LEVEL = resIds.length - 1;
     public Enemy(int level, int index) {
-        super(resIds[level]);
+        super(resIds[level], 10);
         setPosition(Metrics.width / 10 * (2 * index + 1), -RADIUS, RADIUS);
         dy = SPEED;
     }
