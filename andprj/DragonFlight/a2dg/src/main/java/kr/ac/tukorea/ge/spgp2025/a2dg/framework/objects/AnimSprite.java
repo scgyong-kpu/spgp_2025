8803e4724daab03e70f55b8ef0b4e971d616005e
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-20 22:40:06 +0900

    a2dg: 생성자 관련 정리를 해 본다 #1. Sprite 를 상속하는 class AnimSprite 를 만들어 본다. 에러가 난다. Sprite(int) 가 꼭 필요하다.

---
+++ andprj/DragonFlight/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/objects/AnimSprite.java
@@ -0,0 +1,7 @@
+package kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects;
+
+public class AnimSprite extends Sprite {
+    // 다음 에러가 난다.
+    // There is no parameterless constructor available in
+    //  'kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite'
+}
