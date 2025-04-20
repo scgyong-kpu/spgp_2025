85486aafc49ae162f7de804bd3c6f554be1862c8
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-21 05:54:21 +0900

    a2dg: 충돌체크 2. Helper 에 AABB 충돌체크를 구현한다.

--- andprj/DragonFlight/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/util/CollisionHelper.java
+++ andprj/DragonFlight/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/util/CollisionHelper.java
@@ -1,9 +1,18 @@
 package kr.ac.tukorea.ge.spgp2025.a2dg.framework.util;
 
+import android.graphics.RectF;
+
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IGameObject;
 
 public class CollisionHelper {
     public static boolean collides(IGameObject obj1, IGameObject obj2) {
         return false;
+    }
+    public static boolean collides(RectF r1, RectF r2) {
+        if (r1.left > r2.right) return false;
+        if (r1.top > r2.bottom) return false;
+        if (r1.right < r2.left) return false;
+        if (r1.bottom < r2.top) return false;
+        return true;
     }
 }
