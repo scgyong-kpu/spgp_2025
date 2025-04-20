e813408905b17ddf7e85d6161049e67acdbb9ed3
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-20 22:43:35 +0900

    a2dg: 생성자#2-1. 문법적으로는 말이 되지만 AnimSprite 를 만들기 위해 resId 를 전달해야 하는 것이 맞다.

--- andprj/DragonFlight/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/objects/AnimSprite.java
+++ andprj/DragonFlight/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/objects/AnimSprite.java
@@ -1,7 +1,7 @@
 package kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects;
 
 public class AnimSprite extends Sprite {
-    public AnimSprite(int mipmapId) {
-        super(mipmapId);
+    public AnimSprite() {
+        super(R.mipmap.f_01_01); // 이런 형식도 가능하지만 a2dg 에서 app 을 볼 수 없다.
     }
 }
