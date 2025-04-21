da51a5d4b254f3a2d3b8584a6c66ea159dad139b
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-21 23:41:47 +0900

    아래로 스크롤하기.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Background.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Background.java
@@ -1,12 +1,24 @@
 package kr.ac.tukorea.ge.scgyong.dragonflight.game;
 
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
 public class Background extends Sprite {
-    public Background(int bitmapResId) {
+    private final float speed;
+    private final float height;
+    public Background(int bitmapResId, float speed) {
         super(bitmapResId);
-        float height = bitmap.getHeight() * Metrics.width / bitmap.getWidth();
+        this.height = bitmap.getHeight() * Metrics.width / bitmap.getWidth();
         setPosition(Metrics.width / 2, Metrics.height / 2, Metrics.width, height);
+        this.speed = speed;
+    }
+    @Override
+    public void update() {
+        this.y += speed * GameView.frameTime;
+        if (this.y > Metrics.height) {
+            this.y = 0;
+        }
+        setPosition(x, y, Metrics.width, height);
     }
 }
