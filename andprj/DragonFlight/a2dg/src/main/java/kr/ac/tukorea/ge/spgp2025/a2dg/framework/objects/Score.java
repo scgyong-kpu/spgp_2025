c3f1ab3f77de834e0f53612b6cd7ae0c3989dfd8
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-21 21:53:17 +0900

    a2dg: Score 보여주는 객체 추가

---
+++ andprj/DragonFlight/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/objects/Score.java
@@ -0,0 +1,50 @@
+package kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IGameObject;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.res.BitmapPool;
+
+public class Score implements IGameObject {
+    private final Bitmap bitmap;
+    private final float right, top, dstCharWidth, dstCharHeight;
+    private final Rect srcRect = new Rect();
+    private final RectF dstRect = new RectF();
+    private final int srcCharWidth, srcCharHeight;
+    private int score;
+
+    public Score(int mipmapId, float right, float top, float width) {
+        this.bitmap = BitmapPool.get(mipmapId);
+        this.right = right;
+        this.top = top;
+        this.dstCharWidth = width;
+        this.srcCharWidth = bitmap.getWidth() / 10;
+        this.srcCharHeight = bitmap.getHeight();
+        this.dstCharHeight = dstCharWidth * srcCharHeight / srcCharWidth;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        int value = this.score;
+        float x = right;
+        while (value > 0) {
+            int digit = value % 10;
+            srcRect.set(digit * srcCharWidth, 0, (digit + 1) * srcCharWidth, srcCharHeight);
+            x -= dstCharWidth;
+            dstRect.set(x, top, x + dstCharWidth, top + dstCharHeight);
+            canvas.drawBitmap(bitmap, srcRect, dstRect, null);
+            value /= 10;
+        }
+    }
+}
--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/MainScene.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/MainScene.java
@@ -5,15 +5,19 @@ import android.view.MotionEvent;
 
 import java.util.ArrayList;
 
+import kr.ac.tukorea.ge.scgyong.dragonflight.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IGameObject;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Score;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.util.CollisionHelper;
 
 public class MainScene extends Scene {
     private static final String TAG = MainScene.class.getSimpleName();
     private final Fighter fighter;
+    private final Score score;
+
     public enum Layer {
-        enemy, bullet, fighter, controller;
+        enemy, bullet, fighter, ui, controller;
         public static final int COUNT = values().length;
     }
     public MainScene() {
@@ -21,6 +25,11 @@ public class MainScene extends Scene {
 
         this.fighter = new Fighter();
         add(Layer.fighter, fighter);
+
+        this.score = new Score(R.mipmap.number_24x32, 850f, 50f, 60f);
+        score.setScore(12345);
+        add(Layer.ui, score);
+
         add(Layer.controller, new EnemyGenerator());
     }
 
