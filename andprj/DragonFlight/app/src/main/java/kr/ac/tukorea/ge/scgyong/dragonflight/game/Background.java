3d4817df125af63d8acac55bd1b10b77f5b65abe
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-21 23:44:45 +0900

    반복하여 그리기.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Background.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Background.java
@@ -1,5 +1,7 @@
 package kr.ac.tukorea.ge.scgyong.dragonflight.game;
 
+import android.graphics.Canvas;
+
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
@@ -15,10 +17,18 @@ public class Background extends Sprite {
     }
     @Override
     public void update() {
-        this.y += speed * GameView.frameTime;
-        if (this.y > Metrics.height) {
-            this.y = 0;
+        this.y += speed * GameView.frameTime; // y 값을 스크롤된 양으로 사용한다
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        //super.draw(canvas);
+        float curr = y % height;
+        if (curr > 0) curr -= height;
+        while (curr < Metrics.height) {
+            dstRect.set(0, curr, Metrics.width, curr + height);
+            canvas.drawBitmap(bitmap, null, dstRect, null);
+            curr += height;
         }
-        setPosition(x, y, Metrics.width, height);
     }
 }
