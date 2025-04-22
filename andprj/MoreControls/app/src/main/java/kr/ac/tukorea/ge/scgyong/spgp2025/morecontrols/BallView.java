955966ced813953e406302ae7b18ddda5f70d9a0
gogwangsin <gwangsin1029@naver.com>
2025-04-22 18:07:52 +0900

    복습3

--- andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/BallView.java
+++ andprj/MoreControls/app/src/main/java/kr/ac/tukorea/ge/scgyong/spgp2025/morecontrols/BallView.java
@@ -73,22 +73,37 @@ public class BallView extends View {
 
     // 뷰의 크기가 바뀔 때마다 호출된다
     // 뷰의 크기(w, h)에 맞춰서 **이미지 비율을 맞추고 변환 행렬**을 설정한다.
+    //
+    // w, h: View의 실제 픽셀 크기 (onSizeChanged에서 전달됨)
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
 
         float view_ratio = (float)w / (float)h; // 현재 뷰의 가로 세로 비율
-        float game_ratio = SCREEN_WIDTH / SCREEN_HEIGHT; // 정의한 게임 화면의 가로 세로 비율
+        float game_ratio = SCREEN_WIDTH / SCREEN_HEIGHT; // 정의한 게임 화면의 가로 세로 비율 (9/16)
 
+        // 게임 화면 뷰를 9:16이라고 했을 때
+        //
         // 만약 뷰의 비율이 게임화면비율보다 크면, 세로 크기에 맞게 이미지를 스케일하고,
         // 나머지 공간은 좌우 여백으로 맞추고, 반대로 가로 크기에 맞춰 이미지를 스케일하고 상하 여백을 맞춘다.
         transformMatrix.reset();
         if (view_ratio > game_ratio) {
+            // 뷰의 비율이 게임보다 넓다 ---> 가로가 남고 세로가 꽉 찰 것이다.
+
             float scale = h / SCREEN_HEIGHT;
+            // -> 세로 기준으로 맞춰서 확대/축소할 비율을 정한다.
+
             transformMatrix.setTranslate((w - h * game_ratio) / 2, 0);
+            // 가로가 남기 때문에, 양쪽 여백의 반만큼 왼쪽으로 이동해서 가운데 정렬
+
             transformMatrix.preScale(scale, scale);
+            // scale만큼 전체 좌표계 확대 ( 게임 좌표계 -> 화면 좌표계 )
         } else {
             float scale = w / SCREEN_WIDTH;
+            // → 가로 기준으로 맞춰서 확대/축소할 비율을 정함.
+
             transformMatrix.setTranslate(0, (h - w / game_ratio) / 2);
+            // → 세로가 남기 때문에, 위아래 여백의 반만큼 아래로 이동해서 가운데 정렬시킴.
+
             transformMatrix.preScale(scale, scale);
         }
     }
@@ -102,8 +117,13 @@ public class BallView extends View {
         canvas.save();
         canvas.concat(transformMatrix);
         // 변환 행렬을 캔버스에 적용하여 이미지를 적절하게 변형
-
+        //
+        // setMatrix()를 사용하는 대신, 캔버스의 현재 행렬과 지정된 행렬을 결합하여 새로운 변환을 적용할 수 있는
+        // concat() 메소드를 사용하는 것이 더 직관적일 수 있습니다. 이 메소드는 캔버스의 현재 행렬과 지정된 행렬을
+        // 연결(concatenate)합니다. 이는 이후의 그리기 작업에 결합된 변환을 적용합니다. 이 접근 방식은 현재 상태의
+        // 캔버스에 추가 변환을 적용하고자 할 때 종종 더 직관적입니다.
         // 이미지를 해당 위치와 크기에 맞춰 그린다
+
         canvas.drawBitmap(bgBitmap, null, bgRect, null);
         canvas.drawBitmap(ballBitmap, null, ballRect, null);
         canvas.restore();
