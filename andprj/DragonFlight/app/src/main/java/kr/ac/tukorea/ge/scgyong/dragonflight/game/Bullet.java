7e6c94d7089a100d8c139d494a118b5e43f8707e
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-28 22:20:39 +0900

    Bullet 도 생성자를 public 으로 만들고 실질적생성자를 init 로 만든다.

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Bullet.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Bullet.java
@@ -13,18 +13,18 @@ public class Bullet extends Sprite implements IRecyclable, IBoxCollidable, ILaye
     private static final float BULLET_WIDTH = 68f;
     private static final float BULLET_HEIGHT = BULLET_WIDTH * 40 / 28;
     private static final float SPEED = 2000f;
-    private Bullet(float x, float y) {
+    public Bullet() {
         super(R.mipmap.laser_1);
-        setPosition(x, y, BULLET_WIDTH, BULLET_HEIGHT);
         dy = -SPEED;
     }
+
+    private Bullet init(float x, float y) {
+        setPosition(x, y, BULLET_WIDTH, BULLET_HEIGHT);
+        return this;
+    }
     public static Bullet get(float x, float y) {
-        Bullet bullet = (Bullet) Scene.top().getRecyclable(Bullet.class);
-        if (bullet == null) {
-            bullet = new Bullet(x, y);
-        } else {
-            bullet.setPosition(x, y, BULLET_WIDTH, BULLET_HEIGHT);
-        }
+        Bullet bullet = Scene.top().getRecyclable(Bullet.class);
+        bullet.init(x, y);
         return bullet;
     }
     @Override
