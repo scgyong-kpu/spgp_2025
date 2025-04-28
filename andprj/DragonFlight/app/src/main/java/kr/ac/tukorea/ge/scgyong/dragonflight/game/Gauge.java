57a77f4dbf92cc5cbc5b350e9367bd5f6f02974d
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-04-28 22:29:50 +0900

    StrokeCap 을 ROUND 로 변경한다

--- andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Gauge.java
+++ andprj/DragonFlight/app/src/main/java/kr/ac/tukorea/ge/scgyong/dragonflight/game/Gauge.java
@@ -12,9 +12,11 @@ public class Gauge {
         bgPaint.setStyle(Paint.Style.STROKE);
         bgPaint.setStrokeWidth(20f);
         bgPaint.setColor(Color.YELLOW);
+        bgPaint.setStrokeCap(Paint.Cap.ROUND);
         fgPaint.setStyle(Paint.Style.STROKE);
         fgPaint.setStrokeWidth(10f);
         fgPaint.setColor(Color.BLUE);
+        fgPaint.setStrokeCap(Paint.Cap.ROUND);
     }
     public void draw(Canvas canvas) {
         canvas.drawLine(100, 500, 200, 500, bgPaint);
