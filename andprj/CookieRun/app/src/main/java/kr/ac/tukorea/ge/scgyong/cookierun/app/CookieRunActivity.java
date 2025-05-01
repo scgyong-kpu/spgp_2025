669dbf303b75ef31e247b00cde247e2a9332fe5e
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-01 16:26:10 +0900

    MainScene - CookieRunActivity 실행구조 만들고 빈 Scene 하나 push 해 넣음.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/CookieRunActivity.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/CookieRunActivity.java
@@ -2,18 +2,15 @@ package kr.ac.tukorea.ge.scgyong.cookierun.app;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
-import kr.ac.tukorea.ge.scgyong.cookierun.R;
-
-public class CookieRunActivity extends AppCompatActivity {
+public class CookieRunActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        new Scene().push();
     }
 }
