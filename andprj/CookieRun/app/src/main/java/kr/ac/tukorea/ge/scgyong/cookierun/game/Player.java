5ca9a0f215f4f34b81202830290c4121b02c251b
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-01 17:30:45 +0900

    Sheet Animation 으로 만든다. 좌표를 지정하는 방식에 주목해 본다.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/Player.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/Player.java
@@ -1,5 +1,7 @@
 package kr.ac.tukorea.ge.scgyong.cookierun.game;
 
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 
 import kr.ac.tukorea.ge.scgyong.cookierun.R;
@@ -9,21 +11,37 @@ public class Player extends AnimSprite {
     public enum State {
         running, jump
     }
-    protected State state = State.jump;
+    protected State state = State.running;
+    protected static Rect[][] srcRectsArray = {
+            new Rect[] {
+                    new Rect(72 + 0 * 272, 404, 72+140 + 0 * 272, 404+140),
+                    new Rect(72 + 1 * 272, 404, 72+140 + 1 * 272, 404+140),
+                    new Rect(72 + 2 * 272, 404, 72+140 + 2 * 272, 404+140),
+                    new Rect(72 + 3 * 272, 404, 72+140 + 3 * 272, 404+140)
+            },
+            new Rect[] {
+                    new Rect(72 + 7 * 272, 132, 72+140 + 7 * 272, 132+140),
+                    new Rect(72 + 8 * 272, 132, 72+140 + 8 * 272, 132+140),
+            },
+    };
     public Player() {
-        super(R.mipmap.cookie_player_jump, 8);
+        super(R.mipmap.cookie_player_sheet, 8);
         setPosition(200f, 700f, 200f, 200f);
     }
     public void jump() {
         if (state == State.running) {
-            setImageResourceId(R.mipmap.cookie_player_jump, 8, 2);
-            setPosition(x, y, 180f, 200f);
             state = State.jump;
         } else {
-            setImageResourceId(R.mipmap.cookie_player_run, 8, 4);
-            setPosition(x, y, 200f, 200f);
             state = State.running;
         }
+    }
+    @Override
+    public void draw(Canvas canvas) {
+        long now = System.currentTimeMillis();
+        float time = (now - createdOn) / 1000.0f;
+        Rect[] rects = srcRectsArray[state.ordinal()];
+        int frameIndex = Math.round(time * fps) % rects.length;
+        canvas.drawBitmap(bitmap, rects[frameIndex], dstRect, null);
     }
     public boolean onTouch(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
