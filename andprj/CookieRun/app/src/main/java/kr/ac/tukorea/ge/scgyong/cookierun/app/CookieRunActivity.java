87d98e5de9d3b1624732fade64d39b7aa5e2ec11
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-01 16:29:15 +0900

    Debug Build 일 때 border, fps, objcount 보여주려면 new GameView 이전에 debug 설정을 해 준다.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/CookieRunActivity.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/CookieRunActivity.java
@@ -2,15 +2,17 @@ package kr.ac.tukorea.ge.scgyong.cookierun.app;
 
 import android.os.Bundle;
 
+import kr.ac.tukorea.ge.scgyong.cookierun.BuildConfig;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 
 public class CookieRunActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        GameView.drawsDebugStuffs = BuildConfig.DEBUG;
         super.onCreate(savedInstanceState);
-
         new Scene().push();
     }
 }
