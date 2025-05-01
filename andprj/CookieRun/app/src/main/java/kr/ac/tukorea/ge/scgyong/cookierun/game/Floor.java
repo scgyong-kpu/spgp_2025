7e9c1de41ad75e78918a660e8d919e66aff5ccd2
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-02 02:14:26 +0900

    Floor 는 x,y 를 사용하지 않고 dstRect 만을 사용하도록 한다. 10x2, 2x2, 3x1 의 세 개의 타입을 두고, 몇 개의 Platform 객체를 Scene 에 추가한다.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/Floor.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/Floor.java
@@ -2,10 +2,26 @@ package kr.ac.tukorea.ge.scgyong.cookierun.game;
 
 import kr.ac.tukorea.ge.scgyong.cookierun.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.res.BitmapPool;
 
 public class Floor extends Sprite {
-    public Floor() {
-        super(R.mipmap.cookierun_floor_480x48);
-        setPosition(600f, 800f, 1000f, 200f);
+    public enum Type {
+        T_10x2, T_2x2, T_3x1
+    }
+    protected static int[] resIds = {
+            R.mipmap.cookierun_floor_480x48,
+            R.mipmap.cookierun_floor_124x120,
+            R.mipmap.cookierun_floor_120x40,
+    };
+    protected static int[][] sizes = {
+            { 1000, 200 }, { 200, 200 }, { 300, 100 }
+    };
+    public Floor(Type type, float left, float top) {
+        super(0);
+        int ord = type.ordinal();
+        bitmap = BitmapPool.get(resIds[ord]);
+        width = sizes[ord][0];
+        height = sizes[ord][1];
+        dstRect.set(left, top, left + width, top + height);
     }
 }
