dbdf70873fec6d0850cde648708cf24a5c5165f7
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-01 16:21:42 +0900

    "app" package -> New -> Activity -> Empty Views Activity -> "CookieRunActivity" -> layout xml 생성 해제

---
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/CookieRunActivity.java
@@ -0,0 +1,19 @@
+package kr.ac.tukorea.ge.scgyong.cookierun.app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import kr.ac.tukorea.ge.scgyong.cookierun.R;
+
+public class CookieRunActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
