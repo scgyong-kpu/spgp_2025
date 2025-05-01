8c8bb3ae1c7e7e7b1e49bca2c1208fc42839f1a8
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-01 16:48:57 +0900

    MainActivity 대신 CookieRunActivity 가 Launcher category 를 가지게 하면 이 앱의 기본 activity 가 되게 할 수도 있지만, 이 방법은 쓰지 않을 것이다.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/MainActivity.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/MainActivity.java
@@ -20,9 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (BuildConfig.DEBUG) {
-            startActivity(new Intent(this, CookieRunActivity.class));
-        }
     }
 
     public void onBtnStartGame(View view) {
