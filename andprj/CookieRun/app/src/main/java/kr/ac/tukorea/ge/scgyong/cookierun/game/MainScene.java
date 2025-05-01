6ad9dc02c8d32fa73f90f374cff0facb39bea59a
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-01 17:00:11 +0900

    Vert* 대신 Horz* 를 사용하도록 수정하였지만, 아직 Horz 를 제대로 구현한 것은 아니므로 나중에 적용될 것이다.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/MainScene.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/MainScene.java
@@ -1,7 +1,7 @@
 package kr.ac.tukorea.ge.scgyong.cookierun.game;
 
 import kr.ac.tukorea.ge.scgyong.cookierun.R;
-import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.VertScrollBackground;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.HorzScrollBackground;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
 public class MainScene extends Scene {
@@ -13,8 +13,8 @@ public class MainScene extends Scene {
     public MainScene() {
         initLayers(Layer.COUNT);
 
-        add(Layer.bg, new VertScrollBackground(R.mipmap.cookie_run_bg_1, 100f));
-        add(Layer.bg, new VertScrollBackground(R.mipmap.cookie_run_bg_2, 200f));
-        add(Layer.bg, new VertScrollBackground(R.mipmap.cookie_run_bg_3, 300f));
+        add(Layer.bg, new HorzScrollBackground(R.mipmap.cookie_run_bg_1, 100f));
+        add(Layer.bg, new HorzScrollBackground(R.mipmap.cookie_run_bg_2, 200f));
+        add(Layer.bg, new HorzScrollBackground(R.mipmap.cookie_run_bg_3, 300f));
     }
 }
