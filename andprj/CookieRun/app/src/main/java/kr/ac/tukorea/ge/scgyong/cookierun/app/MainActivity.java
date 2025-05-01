96e897856cf8b92f414de26f4718a0176192b31c
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-01 16:46:46 +0900

    디버그할때는 버튼 누르기가 귀찮다. MainActivity 가 생성되자마자 바로 CookieRunActivity 가 실행되게 하자.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/MainActivity.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import kr.ac.tukorea.ge.scgyong.cookierun.BuildConfig;
 import kr.ac.tukorea.ge.scgyong.cookierun.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (BuildConfig.DEBUG) {
+            startActivity(new Intent(this, CookieRunActivity.class));
+        }
     }
 
     public void onBtnStartGame(View view) {
