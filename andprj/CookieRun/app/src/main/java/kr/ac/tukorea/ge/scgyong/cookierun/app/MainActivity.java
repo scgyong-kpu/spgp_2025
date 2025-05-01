1652eb7145d0811f32e7f9027051994a755a2f78
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-01 16:20:32 +0900

    Android Project View -> cookierun -> RightClick/Add/package -> app 과 game 생성 -> MainActivity 를 app/ 로 Drag&Drop. 혹은 그냥 MainActivity 에 오른쪽 버튼 누르고 Move Class 한 뒤 .app 추가, 패키지 생성 확인.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/MainActivity.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/MainActivity.java
@@ -1,4 +1,4 @@
-package kr.ac.tukorea.ge.scgyong.cookierun;
+package kr.ac.tukorea.ge.scgyong.cookierun.app;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import kr.ac.tukorea.ge.scgyong.cookierun.R;
 
 public class MainActivity extends AppCompatActivity {
 
