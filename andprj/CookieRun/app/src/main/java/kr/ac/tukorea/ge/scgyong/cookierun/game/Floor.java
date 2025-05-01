2edea4a44cabab09362b9a8d83ff68f60b5b9bf1
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-02 02:20:21 +0900

    Platform.Type 에 의해 결정되는 정보들은 Platform.Type 이 관리하도록 한다

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/Floor.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/Floor.java
@@ -1,27 +1,32 @@
 package kr.ac.tukorea.ge.scgyong.cookierun.game;
 
+import android.graphics.Bitmap;
+
 import kr.ac.tukorea.ge.scgyong.cookierun.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.res.BitmapPool;
 
 public class Floor extends Sprite {
     public enum Type {
-        T_10x2, T_2x2, T_3x1
+        T_10x2, T_2x2, T_3x1;
+        Bitmap bitmap() { return BitmapPool.get(resId()); }
+        int resId() { return resIds[this.ordinal()]; }
+        int width() { return sizes[this.ordinal()][0]; }
+        int height() { return sizes[this.ordinal()][1]; }
+        static final int[] resIds = {
+                R.mipmap.cookierun_floor_480x48,
+                R.mipmap.cookierun_floor_124x120,
+                R.mipmap.cookierun_floor_120x40,
+        };
+        static final int[][] sizes = {
+                { 1000, 200 }, { 200, 200 }, { 300, 100 }
+        };
     }
-    protected static int[] resIds = {
-            R.mipmap.cookierun_floor_480x48,
-            R.mipmap.cookierun_floor_124x120,
-            R.mipmap.cookierun_floor_120x40,
-    };
-    protected static int[][] sizes = {
-            { 1000, 200 }, { 200, 200 }, { 300, 100 }
-    };
     public Floor(Type type, float left, float top) {
         super(0);
-        int ord = type.ordinal();
-        bitmap = BitmapPool.get(resIds[ord]);
-        width = sizes[ord][0];
-        height = sizes[ord][1];
+        bitmap = type.bitmap();
+        width = type.width();
+        height = type.height();
         dstRect.set(left, top, left + width, top + height);
     }
 }
