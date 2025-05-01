e5c4e6fabd0ccc6d397ef6833b9430ce0e91eaf9
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-01 17:13:15 +0900

    AnimSprite 를 상속하도록 바꾸어 본다. 초당 8장으로 애니메이션 해보지만 이상하게 나온다.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/Player.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/Player.java
@@ -1,11 +1,11 @@
 package kr.ac.tukorea.ge.scgyong.cookierun.game;
 
 import kr.ac.tukorea.ge.scgyong.cookierun.R;
-import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.AnimSprite;
 
-public class Player extends Sprite {
+public class Player extends AnimSprite {
     public Player() {
-        super(R.mipmap.cookie_player);
+        super(R.mipmap.cookie_player_run, 8);
         setPosition(200f, 700f, 200f, 200f);
     }
 }
