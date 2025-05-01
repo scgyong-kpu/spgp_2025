8a99063a4c54209aceb6359f063091d86e48418e
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-01 16:30:57 +0900

    화면좌표계를 1600x900 으로 설정한다.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/CookieRunActivity.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/CookieRunActivity.java
@@ -6,12 +6,14 @@ import kr.ac.tukorea.ge.scgyong.cookierun.BuildConfig;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
 public class CookieRunActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         GameView.drawsDebugStuffs = BuildConfig.DEBUG;
+        Metrics.setGameSize(1600, 900);
         super.onCreate(savedInstanceState);
         new Scene().push();
     }
