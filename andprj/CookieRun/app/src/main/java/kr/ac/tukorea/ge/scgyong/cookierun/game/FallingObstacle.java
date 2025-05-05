6660936bd3eca36bc68e649e5b1f1beb82059cc6
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-06 02:48:56 +0900

    ValueAnimator 가 넘겨준 값을 dstRect 의 y 값으로 적용시킨다. 시작시각을 1000ms 지연시켜본다.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/FallingObstacle.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/FallingObstacle.java
@@ -1,15 +1,12 @@
 package kr.ac.tukorea.ge.scgyong.cookierun.game;
 
 import android.animation.ValueAnimator;
-import android.util.Log;
 
 import kr.ac.tukorea.ge.scgyong.cookierun.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
-import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 
 public class FallingObstacle extends Obstacle {
     private static final int RES_ID = R.mipmap.epn01_tm01_sda;
-    private static final float FALL_SPEED = 500f;
     private static final String TAG = FallingObstacle.class.getSimpleName();
 
     public FallingObstacle() {
@@ -26,12 +23,12 @@ public class FallingObstacle extends Obstacle {
         dstRect.offset(0, -dstRect.height());
         float start = dstRect.top;
 
-        Log.d(TAG, "--- Anim(" + start + " -> " + end + ")");
         ValueAnimator animator = ValueAnimator.ofFloat(start, end);
         animator.setDuration(2000);
+        animator.setStartDelay(1000);
         animator.addUpdateListener(animation -> {
             float value = (float) animation.getAnimatedValue();
-            Log.v(TAG, "Animated Value = " + value);
+            dstRect.offsetTo(dstRect.left, value);
         });
         animator.start();
 
