a82a15ef7b0eed8f9850fc69965e27415d40a119
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-06 03:00:28 +0900

    Animator 및 interpolator, listener 의 생성을 객체당 한번만 하도록 한다

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/FallingObstacle.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/FallingObstacle.java
@@ -3,12 +3,15 @@ package kr.ac.tukorea.ge.scgyong.cookierun.game;
 import android.animation.ValueAnimator;
 import android.view.animation.BounceInterpolator;
 
+import java.util.function.Consumer;
+
 import kr.ac.tukorea.ge.scgyong.cookierun.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
 public class FallingObstacle extends Obstacle {
     private static final int RES_ID = R.mipmap.epn01_tm01_sda;
     private static final String TAG = FallingObstacle.class.getSimpleName();
+    private ValueAnimator animator;
 
     public FallingObstacle() {
         setImageResourceId(RES_ID);
@@ -24,16 +27,25 @@ public class FallingObstacle extends Obstacle {
         dstRect.offset(0, -dstRect.height());
         float start = dstRect.top;
 
-        ValueAnimator animator = ValueAnimator.ofFloat(start, end);
-        animator.setDuration(2000);
-        animator.setStartDelay(1000);
-        animator.setInterpolator(new BounceInterpolator());
-        animator.addUpdateListener(animation -> {
-            float value = (float) animation.getAnimatedValue();
-            dstRect.offsetTo(dstRect.left, value);
-        });
-        animator.start();
+        initAnimator(); // 생성이 안 되어 있다면 생성한다
+        animator.setFloatValues(start, end); // 시작/끝 값을 지정한다
+        animator.start(); // 시작한다
 
         return this;
     }
+
+    private void initAnimator() {
+        if (animator != null) return;
+        // 모든 FallingObstacle 객체가 동일하게 적용받는 항목은 이곳에 적는다
+        animator = new ValueAnimator();
+        animator.setDuration(2000);
+        animator.setStartDelay(1000);
+        animator.setInterpolator(new BounceInterpolator());
+        animator.addUpdateListener(animListener);
+    }
+
+    private final ValueAnimator.AnimatorUpdateListener animListener = (ValueAnimator anim) -> {
+        float value = (float) anim.getAnimatedValue();
+        dstRect.offsetTo(dstRect.left, value);
+    };
 }
