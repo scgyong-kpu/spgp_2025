21f6bd35ce7903743be78502bd113957d45f5d24
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-09 10:11:31 +0900

    Stage Number 를 Intent 의 extra 를 통해 전달하는 구조. 일단 stage 가 전달되는것만 확인하고 MainScene 대신 빈 Scene 을 넣음.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/CookieRunActivity.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/CookieRunActivity.java
@@ -1,10 +1,12 @@
 package kr.ac.tukorea.ge.scgyong.cookierun.app;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import kr.ac.tukorea.ge.scgyong.cookierun.BuildConfig;
 import kr.ac.tukorea.ge.scgyong.cookierun.game.MainScene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
@@ -15,6 +17,9 @@ public class CookieRunActivity extends GameActivity {
         GameView.drawsDebugStuffs = BuildConfig.DEBUG;
         Metrics.setGameSize(1600, 900);
         super.onCreate(savedInstanceState);
-        new MainScene().push();
+        int stage = getIntent().getIntExtra("stage", 1);
+        Log.d(CookieRunActivity.class.getSimpleName(), "Stage = " + stage);
+        //new MainScene().push();
+        new Scene().push();
     }
 }
--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/MainActivity.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/app/MainActivity.java
@@ -2,35 +2,50 @@ package kr.ac.tukorea.ge.scgyong.cookierun.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import kr.ac.tukorea.ge.scgyong.cookierun.BuildConfig;
 import kr.ac.tukorea.ge.scgyong.cookierun.R;
+import kr.ac.tukorea.ge.scgyong.cookierun.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding ui;
+    private int stage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (BuildConfig.DEBUG) {
-            startActivity(new Intent(this, CookieRunActivity.class));
-        }
+        ui = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(ui.getRoot());
+        setStage(1);
+
+        //if (BuildConfig.DEBUG) {
+        //    startActivity(new Intent(this, CookieRunActivity.class));
+        //}
     }
 
     public void onBtnStartGame(View view) {
-        startGame(1);
+        startGame();
     }
 
-    private void startGame(int stage) {
+    private void startGame() {
         Intent intent = new Intent(this, CookieRunActivity.class);
+        intent.putExtra("stage", stage);
         startActivity(intent);
+    }
+
+    private void setStage(int stage) {
+        this.stage = stage;
+        String text = getString(R.string.title_stage_fmt, stage);
+        ui.stageTextView.setText(text);
+        ui.prevButton.setEnabled(stage > 1);
+        ui.nextButton.setEnabled(stage < 3);
+    }
+
+    public void onBtnPrevious(View view) {
+        setStage(stage - 1);
+    }
+    public void onBtnNext(View view) {
+        setStage(stage + 1);
     }
 }
