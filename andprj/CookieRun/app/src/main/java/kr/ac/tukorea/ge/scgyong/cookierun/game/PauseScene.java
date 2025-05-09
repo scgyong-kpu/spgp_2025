2e254f52eceafff10c11de3a4e98cea066de9630
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-09 10:43:55 +0900

    bg 이미지 크기 줄이고 title 을 애니메이션시킴.

--- andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/PauseScene.java
+++ andprj/CookieRun/app/src/main/java/kr/ac/tukorea/ge/scgyong/cookierun/game/PauseScene.java
@@ -4,17 +4,28 @@ import kr.ac.tukorea.ge.scgyong.cookierun.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Button;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
 public class PauseScene extends Scene {
     public enum Layer {
         bg, title, touch
     }
+    protected float angle = -(float)Math.PI / 2;
     public PauseScene() {
         initLayers(Layer.values().length);
         float w = Metrics.width, h = Metrics.height;
-        add(Layer.bg, new Sprite(R.mipmap.bg_city_landscape, w/2, h/2, w, h));
-        add(Layer.bg, new Sprite(R.mipmap.cookie_run_title, w/2, h/2, 3.69f, 1.36f));
+        add(Layer.bg, new Sprite(R.mipmap.bg_city_landscape, w/2, h/2, 1200f, 675f));
+        add(Layer.title, new Sprite(R.mipmap.cookie_run_title, w/2, h/2, 369f, 136f) {
+            @Override
+            public void update() {
+                super.update();
+                angle -= (float) (GameView.frameTime * Math.PI / 4);
+                float x = (float) (800f + 400f * Math.cos(angle));
+                float y = (float) (450f + 200f * Math.sin(angle));
+                setPosition(x, y, width, height);
+            }
+        });
         add(Layer.touch, new Button(R.mipmap.btn_resume_n, 1450f, 100f, 200f, 75f, new Button.OnTouchListener() {
             @Override
             public boolean onTouch(boolean pressed) {
