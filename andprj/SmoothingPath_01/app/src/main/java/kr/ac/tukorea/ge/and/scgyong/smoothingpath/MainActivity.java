18573a15ee6c4a23a113c3c24c40a37caf6b8a43
KKY@C221 <scgyong@tukorea.ac.kr>
2025-05-13 11:12:20 +0900

    Binding 사용.

--- andprj/SmoothingPath_01/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/smoothingpath/MainActivity.java
+++ andprj/SmoothingPath_01/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/smoothingpath/MainActivity.java
@@ -3,22 +3,23 @@ package kr.ac.tukorea.ge.and.scgyong.smoothingpath;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import kr.ac.tukorea.ge.and.scgyong.smoothingpath.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private @NonNull ActivityMainBinding ui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //setContentView(R.layout.activity_main);
+        ui = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(ui.getRoot());
     }
 }
