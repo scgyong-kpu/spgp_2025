a469b0ef874f554d006396859ea799f0dd1f0dc9
KKY@C221 <scgyong@tukorea.ac.kr>
2025-05-14 16:37:02 +0900

    이벤트로 받은 좌표로 포인트 생성하고 멤버 points 에 추가

--- andprj/SmoothingPath_02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/smoothingpath/PathView.java
+++ andprj/SmoothingPath_02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/smoothingpath/PathView.java
@@ -1,12 +1,15 @@
 package kr.ac.tukorea.ge.and.scgyong.smoothingpath;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class PathView extends View {
     private static final String TAG = PathView.class.getSimpleName();
@@ -24,11 +27,14 @@ public class PathView extends View {
     private void init() {
     }
 
+    private ArrayList<PointF> points = new ArrayList<>();
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         float x = event.getX();
         float y = event.getY();
-        Log.d(TAG, "TouchEvent: action=" + event.getAction() + " pos=" + x + "," + y);
+        points.add(new PointF(x, y));
+        Log.d(TAG, "TouchEvent: action=" + event.getAction() + " pos=" + x + "," + y + " now points count=" + points.size());
         return super.onTouchEvent(event);
     }
 }
