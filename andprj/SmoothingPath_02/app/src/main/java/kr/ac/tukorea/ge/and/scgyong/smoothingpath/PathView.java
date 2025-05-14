e46206fa3a7658bc25ba171cb5e11683303e13bb
KKY@C221 <scgyong@tukorea.ac.kr>
2025-05-14 16:22:31 +0900

    PathView  가 View 를 상속하게 하고 필요한 생성자 추가

--- andprj/SmoothingPath_02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/smoothingpath/PathView.java
+++ andprj/SmoothingPath_02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/smoothingpath/PathView.java
@@ -1,4 +1,22 @@
 package kr.ac.tukorea.ge.and.scgyong.smoothingpath;
 
-public class PathView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class PathView extends View {
+    public PathView(Context context) {
+        super(context);
+        init();
+    }
+
+    public PathView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+    }
 }
