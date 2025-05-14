249b9af98c3b51bd48b09d12eb02798e0cee533c
KKY@C221 <scgyong@tukorea.ac.kr>
2025-05-14 16:32:25 +0900

    TOUCH_DOWN 이벤트 받아서 로그 출력.

--- andprj/SmoothingPath_02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/smoothingpath/PathView.java
+++ andprj/SmoothingPath_02/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/smoothingpath/PathView.java
@@ -2,11 +2,15 @@ package kr.ac.tukorea.ge.and.scgyong.smoothingpath;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class PathView extends View {
+    private static final String TAG = PathView.class.getSimpleName();
+
     public PathView(Context context) {
         super(context);
         init();
@@ -18,5 +22,13 @@ public class PathView extends View {
     }
 
     private void init() {
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        Log.d(TAG, "TouchEvent: action=" + event.getAction() + " pos=" + x + "," + y);
+        return super.onTouchEvent(event);
     }
 }
