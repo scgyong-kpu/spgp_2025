58c4ab21257676c3e9577f4611db2cdc7a7bd942
KKY@C221 <scgyong@tukorea.ac.kr>
2025-05-19 11:37:24 +0900

    Animation 의 속도가 일정하도록.

--- andprj/SmoothingPath_01/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/smoothingpath/PathView.java
+++ andprj/SmoothingPath_01/app/src/main/java/kr/ac/tukorea/ge/and/scgyong/smoothingpath/PathView.java
@@ -53,7 +53,7 @@ public class PathView extends View {
         PathMeasure pm = new PathMeasure(path, closesPath);
         float length = pm.getLength();
         ValueAnimator animator = ValueAnimator.ofFloat(0.0f, length);
-        animator.setDuration(1200);
+        animator.setDuration((long)length);
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(@NonNull ValueAnimator animation) {
