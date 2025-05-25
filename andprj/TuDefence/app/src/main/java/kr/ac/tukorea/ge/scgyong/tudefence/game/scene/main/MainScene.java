2ee15ae1545737fc4787c9abd2a9ed09639b04af
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-26 00:32:44 +0900

    TiledBackground class 생성.

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/MainScene.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/MainScene.java
@@ -3,4 +3,11 @@ package kr.ac.tukorea.ge.scgyong.tudefence.game.scene.main;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
 public class MainScene extends Scene {
+    enum Layer {
+        bg
+    }
+    public MainScene() {
+        initLayers(Layer.values().length);
+        add(Layer.bg, new TiledBackground("map/desert.tmj"));
+    }
 }
---
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/TiledBackground.java
@@ -0,0 +1,19 @@
+package kr.ac.tukorea.ge.scgyong.tudefence.game.scene.main;
+
+import android.graphics.Canvas;
+
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IGameObject;
+
+public class TiledBackground implements IGameObject {
+    public TiledBackground(String mapAssetFile) {
+
+    }
+
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+    }
+}
