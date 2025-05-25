90476271b45c0101eeec2757bec87ea94f56a3b4
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-26 00:54:41 +0900

    quicktype.io 가 생성해준 클래스들을 이용하여 asset 내의 json 을 parsing 한다.

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/TiledBackground.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/TiledBackground.java
@@ -1,14 +1,53 @@
 package kr.ac.tukorea.ge.scgyong.tudefence.game.scene.main;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Canvas;
+import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import kr.ac.tukorea.ge.scgyong.tudefence.game.map.Converter;
+import kr.ac.tukorea.ge.scgyong.tudefence.game.map.TiledMap;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IGameObject;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 
 public class TiledBackground implements IGameObject {
-    public TiledBackground(String mapAssetFile) {
+    private static final String TAG = TiledBackground.class.getSimpleName();
+    private final TiledMap map;
 
+    public TiledBackground(String mapAssetFile) {
+        map = loadMap(mapAssetFile);
+        Log.d(TAG, "Map file " + mapAssetFile + " has " + map.getLayers().length + " layer(s) and " + map.getTilesets().length + " tileset(s).");
     }
 
+    private TiledMap loadMap(String fileName) {
+        try {
+            String json = loadAssetAsString(fileName);
+            return Converter.fromJsonString(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    private String loadAssetAsString(String fileName) throws IOException {
+        Context context = GameView.view.getContext();
+        AssetManager assets = context.getAssets();
+        InputStream inputStream = assets.open(fileName);
+        InputStreamReader in = new InputStreamReader(inputStream);
+        BufferedReader reader = new BufferedReader(in);
+        StringBuilder builder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line).append("\n");
+        }
+        reader.close();
+        inputStream.close();
+        return builder.toString();
+    }
     @Override
     public void update() {
     }
