4c0afae794fa311a1533abdf31b8be35af8bacb3
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-25 23:36:58 +0900

    "..tudefence" package -> New -> Activity -> Empty Views Activity -> "MainGameActivity" -> layout xml 생성 해제 -> package 에 .app 추가

---
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/app/MainGameActivity.java
@@ -0,0 +1,19 @@
+package kr.ac.tukorea.ge.scgyong.tudefence.app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import kr.ac.tukorea.ge.scgyong.tudefence.R;
+
+public class MainGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
