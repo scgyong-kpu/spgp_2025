7cf25b1de0dbb60bbe851223e3669294fa25c2bb
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-25 23:45:52 +0900

    MainActivity 에서 화면을 터치하면 MainGameActivity 로 넘어가도록.

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/app/MainActivity.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/app/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.tukorea.ge.scgyong.tudefence.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +17,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            startGameActivity();
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void startGameActivity() {
+        Intent intent = new Intent(this, MainGameActivity.class);
+        startActivity(intent);
     }
 }
