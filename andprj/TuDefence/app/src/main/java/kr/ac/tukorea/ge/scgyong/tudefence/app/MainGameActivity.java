b2c5ab769ab373c39d538a7c4504c31a4c6ac703
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-25 23:38:40 +0900

    MainGameActivity 는 GameActivity 를 상속하도록. Activity 생성후에는 빈 Scene 을 생성하여 push 한다.

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/app/MainGameActivity.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/app/MainGameActivity.java
@@ -2,18 +2,14 @@ package kr.ac.tukorea.ge.scgyong.tudefence.app;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
-import kr.ac.tukorea.ge.scgyong.tudefence.R;
-
-public class MainGameActivity extends AppCompatActivity {
+public class MainGameActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new Scene().push();
     }
 }
