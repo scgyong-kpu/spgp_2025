d600be439e80f19304b6048b71763c27aff091d5
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-25 23:42:51 +0900

    MainActivity 도 .app package 로 이동.

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/app/MainActivity.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/app/MainActivity.java
@@ -1,4 +1,4 @@
-package kr.ac.tukorea.ge.scgyong.tudefence;
+package kr.ac.tukorea.ge.scgyong.tudefence.app;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import kr.ac.tukorea.ge.scgyong.tudefence.R;
 
 public class MainActivity extends AppCompatActivity {
 
