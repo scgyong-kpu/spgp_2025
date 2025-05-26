6ba6b93eeec6988b189541c6408a157b4f70fd93
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-27 00:25:39 +0900

    a2dg: Sprite 의 위치(x,y만) 를 정하는 함수 추가

--- andprj/TuDefence/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/objects/Sprite.java
+++ andprj/TuDefence/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/objects/Sprite.java
@@ -43,6 +43,9 @@ public class Sprite implements IGameObject {
         RectUtil.setRect(dstRect, x, y, radius);
 
     }
+    public void setPosition(float x, float y) {
+        RectUtil.setRect(dstRect, x, y, width, height);
+    }
     public void setPosition(float x, float y, float width, float height) {
         this.x = x;
         this.y = y;
