d1899f73b118d7343c3b1db7b53effd22903c0f4
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-30 11:18:22 +0900

    Cannon 생성자에 레벨, 좌표를 전달하고 레벨별 다른 이미지를 사용한다.

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/Cannon.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/Cannon.java
@@ -11,10 +11,14 @@ import kr.ac.tukorea.ge.spgp2025.a2dg.framework.res.BitmapPool;
 public class Cannon extends Sprite {
     private Bitmap barrelBitmap;
     private final RectF barrelRect = new RectF();
-    public Cannon() {
-        super(R.mipmap.f_01_01);
+    private static final int[] BITMAP_IDS = {
+            R.mipmap.f_01_01, R.mipmap.f_02_01,R.mipmap.f_03_01,R.mipmap.f_04_01,R.mipmap.f_05_01,
+            R.mipmap.f_06_01,R.mipmap.f_07_01,R.mipmap.f_08_01,R.mipmap.f_09_01,R.mipmap.f_10_01,
+    };
+    public Cannon(int level, float x, float y) {
+        super(BITMAP_IDS[level]);
         barrelBitmap = BitmapPool.get(R.mipmap.tank_barrel);
-        setPosition(500, 700, 200, 200);
+        setPosition(x, y, 200, 200);
         barrelRect.set(dstRect);
         barrelRect.inset(-150, -150);
     }
--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/MainScene.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/MainScene.java
@@ -13,6 +13,8 @@ public class MainScene extends Scene {
         initLayers(Layer.values().length);
         add(Layer.bg, new TiledBackground("map/desert.tmj", 100, 100));
         add(Layer.controller, new WaveGen(this, 2.0f));
-        add(Layer.cannon, new Cannon());
+        add(Layer.cannon, new Cannon(0, 400, 600));
+        add(Layer.cannon, new Cannon(2, 1500, 500));
+        add(Layer.cannon, new Cannon(5, 700, 1600));
     }
 }
