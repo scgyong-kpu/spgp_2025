4cf8785e52fc5bc884ae36446d994fdb7bcc0e6c
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-30 11:35:13 +0900

    Barrel 이 오른쪽을 쳐다보는 것이 0도 이므로 초기값으로 위를 쳐다보려면 -90 도여야 한다.

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/Cannon.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/Cannon.java
@@ -16,7 +16,7 @@ public class Cannon extends Sprite {
     private final int level;
     private final Bitmap barrelBitmap;
     private final RectF barrelRect = new RectF();
-    private float angle;
+    private float angle = -90;
     private static final int[] BITMAP_IDS = {
             R.mipmap.f_01_01, R.mipmap.f_02_01,R.mipmap.f_03_01,R.mipmap.f_04_01,R.mipmap.f_05_01,
             R.mipmap.f_06_01,R.mipmap.f_07_01,R.mipmap.f_08_01,R.mipmap.f_09_01,R.mipmap.f_10_01,
