fe764964df7e0d20e94886944b302dabc200fddb
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-30 11:34:27 +0900

    Cannon 이 가장 가까운 Fly 를 쳐다보게 한다.

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/Cannon.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/Cannon.java
@@ -4,14 +4,19 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.RectF;
 
+import java.util.ArrayList;
+
 import kr.ac.tukorea.ge.scgyong.tudefence.R;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IGameObject;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.res.BitmapPool;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
 public class Cannon extends Sprite {
     private final int level;
     private final Bitmap barrelBitmap;
     private final RectF barrelRect = new RectF();
+    private float angle;
     private static final int[] BITMAP_IDS = {
             R.mipmap.f_01_01, R.mipmap.f_02_01,R.mipmap.f_03_01,R.mipmap.f_04_01,R.mipmap.f_05_01,
             R.mipmap.f_06_01,R.mipmap.f_07_01,R.mipmap.f_08_01,R.mipmap.f_09_01,R.mipmap.f_10_01,
@@ -27,8 +32,44 @@ public class Cannon extends Sprite {
     }
 
     @Override
+    public void update() {
+        super.update();
+        Fly fly = findNearestFly();
+        if (fly != null) {
+            angle = (float) Math.toDegrees(Math.atan2(fly.getY() - y, fly.getX() - x));
+        }
+    }
+
+    public Fly findNearestFly() {
+        float dist = Float.MAX_VALUE;
+        Fly nearest = null;
+        MainScene scene = (MainScene) Scene.top();
+        ArrayList<IGameObject> flies = scene.objectsAt(MainScene.Layer.enemy);
+        for (IGameObject gameObject: flies) {
+            if (!(gameObject instanceof Fly)) continue;
+            Fly fly = (Fly) gameObject;
+            float fx = fly.getX();
+            float fy = fly.getY();
+            float dx = x - fx;
+            if (dx > dist) continue;
+            float dy = y - fy;
+            if (dy > dist) continue;
+            float d = (float) Math.sqrt(dx * dx + dy * dy);
+            if (dist > d) {
+                dist = d;
+                nearest = fly;
+            }
+        }
+        return nearest;
+    }
+
+    @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
+        // super (탱크) 를 그린 후 rotate (포신) 해야 한다
+        canvas.save();
+        canvas.rotate(angle, x, y);
         canvas.drawBitmap(barrelBitmap, null, barrelRect, null);
+        canvas.restore();
     }
 }
