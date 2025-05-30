e4ac6d072bf030e69c939b1be8698cf4a5e22988
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-05-30 11:09:01 +0900

    Barrel 이미지 추가

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/Cannon.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/Cannon.java
@@ -1,11 +1,26 @@
 package kr.ac.tukorea.ge.scgyong.tudefence.game.scene.main;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.RectF;
+
 import kr.ac.tukorea.ge.scgyong.tudefence.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.res.BitmapPool;
 
 public class Cannon extends Sprite {
+    private Bitmap barrelBitmap;
+    private final RectF barrelRect = new RectF();
     public Cannon() {
         super(R.mipmap.f_01_01);
+        barrelBitmap = BitmapPool.get(R.mipmap.tank_barrel);
         setPosition(500, 700, 200, 200);
+        barrelRect.set(dstRect);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        canvas.drawBitmap(barrelBitmap, null, barrelRect, null);
     }
 }
