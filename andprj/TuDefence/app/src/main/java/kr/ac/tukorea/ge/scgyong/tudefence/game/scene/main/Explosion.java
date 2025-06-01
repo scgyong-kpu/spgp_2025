5690ce8d15ea9ec99bb230f0038470db275ad2d4
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-06-01 15:43:22 +0900

    AnimSprite 의 기능을 사용하는 것은 좋지 않다. Pause 했을 때를 비교해 보면 좋다. 그런데 잔상이 남는데?

--- andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/Explosion.java
+++ andprj/TuDefence/app/src/main/java/kr/ac/tukorea/ge/scgyong/tudefence/game/scene/main/Explosion.java
@@ -1,11 +1,15 @@
 package kr.ac.tukorea.ge.scgyong.tudefence.game.scene.main;
 
+import android.graphics.Canvas;
+
 import kr.ac.tukorea.ge.scgyong.tudefence.R;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces.IRecyclable;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.AnimSprite;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 
 public class Explosion extends AnimSprite implements IRecyclable {
+    protected float time;
     public Explosion() {
         super(R.mipmap.explosion, 20);
     }
@@ -15,18 +19,25 @@ public class Explosion extends AnimSprite implements IRecyclable {
 
     private Explosion init(float x, float y, float radius) {
         setPosition(x, y, radius);
-        createdOn = System.currentTimeMillis();
+        time = 0;
         return this;
     }
 
     @Override
     public void update() {
-        super.update();
-        long now = System.currentTimeMillis();
-        float time = (now - createdOn) / 1000.0f;
-        if (time > 19.0f/20.0f) {
+        time += GameView.frameTime;
+        if (time >= 1.0) {
             Scene.top().remove(MainScene.Layer.explosion, this);
+            return;
         }
+        int frameIndex = Math.round(time * fps) % frameCount;
+        srcRect.set(frameIndex * frameWidth, 0, (frameIndex + 1) * frameWidth, frameHeight);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        // java 에서는 super.super.draw(canvas) 와 같은 호출은 허락하지 않는다.
+        canvas.drawBitmap(bitmap, srcRect, dstRect, null);
     }
 
     @Override
