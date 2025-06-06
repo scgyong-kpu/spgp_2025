7286c0cf7062fbd2d64c81c5b666a51944c9b562
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-06-06 18:10:09 +0900

    viewBinding 을 사용하여 MainActivity 표시

--- andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/MainActivity.java
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/MainActivity.java
@@ -3,18 +3,24 @@ package kr.ac.tukorea.ge.scgyong.taptu;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import kr.ac.tukorea.ge.scgyong.taptu.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private @NonNull ActivityMainBinding ui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        ui = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(ui.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
