385b14d55d106b52c71ba3806b93afe93c2abdac
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-06-06 19:47:07 +0900

    ListView 가 Adapter 에게 물어보는 구조이다:
     1. 몇개니?
     2. i 번째 view 를 다오..
    
     등등등.

--- andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/MainActivity.java
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/MainActivity.java
@@ -1,6 +1,10 @@
 package kr.ac.tukorea.ge.scgyong.taptu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -33,5 +37,29 @@ public class MainActivity extends AppCompatActivity {
         });
 
         songs = new SongLoader(this).loadSongs();
+
+        ui.songsListView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return songs.size();
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                TextView tv = new TextView(MainActivity.this);
+                tv.setText(songs.get(i).toString());
+                return tv;
+            }
+        });
     }
 }
