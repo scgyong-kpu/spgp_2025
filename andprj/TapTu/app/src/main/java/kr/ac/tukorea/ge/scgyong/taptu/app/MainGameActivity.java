288f829f06ced418b745c57c68d6e0e9a711e42e
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-06-08 16:27:49 +0900

    app -> Right Click -> New -> Activity -> Empty Views Activity -> No Layout XML

---
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/app/MainGameActivity.java
@@ -0,0 +1,19 @@
+package kr.ac.tukorea.ge.scgyong.taptu.app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import kr.ac.tukorea.ge.scgyong.taptu.R;
+
+public class MainGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
