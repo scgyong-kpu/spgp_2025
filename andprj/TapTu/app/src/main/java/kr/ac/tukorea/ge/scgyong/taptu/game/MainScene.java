55725ea22bd45d37d0391a456601e4c6c377c38f
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-06-08 18:24:58 +0900

    Bitmap 을 blur 시키는 코드를 넣는다.  (충분히 테스트된 것은 아니며, RenderScript 라는 것은 deprecated 상태이다)

--- andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/game/MainScene.java
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/game/MainScene.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 
 import kr.ac.tukorea.ge.scgyong.taptu.data.Song;
+import kr.ac.tukorea.ge.scgyong.taptu.res.BitmapBlur;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.objects.Sprite;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
@@ -19,7 +20,8 @@ public class MainScene extends Scene {
         Sprite album = new Sprite(0);
         Context context = GameView.view.getContext();
         Bitmap bitmap = song.getThumbnailBitmap(context);
-        album.setBitmap(bitmap);
+        Bitmap blurredCover = BitmapBlur.blurBitmap(context, bitmap);
+        album.setBitmap(blurredCover);
         float x = Metrics.width / 2, y = Metrics.height / 2;
         album.setPosition(x, y, Metrics.width, Metrics.width);
         add(Layer.bg, album);
