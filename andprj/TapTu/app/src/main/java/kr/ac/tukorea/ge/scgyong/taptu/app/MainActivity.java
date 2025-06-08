a2ce64ea2c5dc7ec8f49444a1e370dfa117b627c
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-06-08 16:54:52 +0900

    Activity간 Data 전달. index 를 전달하는 것은 가능하지만 Song 이나 songs 는 곤란하다. Activity 들이 각기 다른 Process 에 있을 가능성 때문인데, 우리는 같은 process 내에 있으므로 꼭 이 방법을 써야 하는 것은 아니다.

--- andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/app/MainActivity.java
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/app/MainActivity.java
@@ -153,6 +153,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnStartGame(View view) {
         Intent intent = new Intent(this, MainGameActivity.class);
+        intent.putExtra(MainGameActivity.KEY_SONG_INDEX, selectedPosition);
         startActivity(intent);
     }
 }
--- andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/app/MainGameActivity.java
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/app/MainGameActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.tukorea.ge.scgyong.taptu.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +14,15 @@ import kr.ac.tukorea.ge.scgyong.taptu.R;
 
 public class MainGameActivity extends AppCompatActivity {
 
+    public static final String KEY_SONG_INDEX = "songIndex";
+    private static final String TAG = MainGameActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        int songIndex = extras.getInt(KEY_SONG_INDEX);
+        Log.d(TAG, "Song Index = " + songIndex);
     }
 }
