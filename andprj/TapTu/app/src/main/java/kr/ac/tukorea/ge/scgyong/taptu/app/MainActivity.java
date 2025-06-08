701c46ed09c2bb1954756b0822865a9613e31dc8
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-06-08 16:31:46 +0900

    Start Game 버튼을 누르면 MainActivity -> MainGameActivity 실행하도록 한다.

--- andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/app/MainActivity.java
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/app/MainActivity.java
@@ -1,9 +1,11 @@
 package kr.ac.tukorea.ge.scgyong.taptu.app;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
@@ -130,5 +132,9 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.i(TAG, "No Song selected");
         }
+    }
+    public void onBtnStartGame(View view) {
+        Intent intent = new Intent(this, MainGameActivity.class);
+        startActivity(intent);
     }
 }
