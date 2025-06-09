d362aebb6713356cf3feff7264391147b914e177
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-06-10 07:04:44 +0900

    Regex 를 사용해서 parse 하는 예제. 이번 예제는 단순히 빈칸으로 split 하기만 하면 되지만 다양한 패턴이 있을 경우에는 RegEx 를 쓰는 것이 좋다.

--- andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/data/Note.java
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/data/Note.java
@@ -1,16 +1,22 @@
 package kr.ac.tukorea.ge.scgyong.taptu.data;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Note {
     public int pret;
     public float time; // in seconds
+    private static final Pattern pattern = Pattern.compile("^N\\s+(\\d+)\\s+(\\d+)\\s*$");
     public static Note parse(String line) {
-        String[] comps = line.split("\\s+");
-        if (comps.length < 3) return null;
-        if (!comps[0].equals("N")) return null;
+        Matcher m = pattern.matcher(line);
+        if (!m.find()) return null;
+        String pret = m.group(1);
+        String millis = m.group(2);
+        if (pret == null || millis == null) return null; // 필요한 코드는 아니지만 null check analyzer 때문에 써준다
 
         Note note = new Note();
-        note.pret = Integer.parseInt(comps[1]);
-        note.time = Integer.parseInt(comps[2]) / 1000.0f;
+        note.pret = Integer.parseInt(pret);
+        note.time = Integer.parseInt(millis) / 1000.0f;
         return note;
     }
 }
