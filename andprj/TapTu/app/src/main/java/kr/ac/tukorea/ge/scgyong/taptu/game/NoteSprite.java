6a847eaad9521a18b4599c675dc7b9f4e692164e
Kiyong Kim <scgyong@tukorea.ac.kr>
2025-06-10 06:15:11 +0900

    10 으로 나누어 보면 일단 화면에 나오기는 한다.

--- andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/game/NoteSprite.java
+++ andprj/TapTu/app/src/main/java/kr/ac/tukorea/ge/scgyong/taptu/game/NoteSprite.java
@@ -23,7 +23,7 @@ public class NoteSprite extends Sprite implements IRecyclable {
     private NoteSprite init(Note note) {
         this.note = note;
         float x = LEFT + note.pret * X_SPACE;
-        float y = note.msec;
+        float y = note.msec / 10.0f;
         setPosition(x, y);
         return this;
     }
